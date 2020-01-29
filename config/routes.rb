56784af232eaa5a9c457d2b8fd428583ce7c84ea
NLCaceres <NLCaceres21@Gmail.com>
2020-01-28 17:23:41 -0800

    Syntax issue with fallback route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   end
 
   get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-  !request.xhr? && request.format.html?
-end
+    !request.xhr? && request.format.html?
+  end
 end
