79863705187e4ef0bffe23e2372847a2581a0bb5
NLCaceres <NLCaceres21@Gmail.com>
2021-05-30 13:48:59 -0700

    Match default port for local dev proxying

--- config/puma.rb
+++ config/puma.rb
@@ -9,8 +9,8 @@ min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+# //? ENV.fetch() works a lot like a basic hashmap in ruby, the block can return a default value.
+port        ENV.fetch("PORT") { 3001 }
 
 # Specifies the `environment` that Puma will run in.
 #
