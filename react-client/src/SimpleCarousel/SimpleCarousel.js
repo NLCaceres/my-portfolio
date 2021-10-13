7a9bd02cc4dc6057addcfe3917ce3e7f7af97e8f
NLCaceres <NLCaceres21@Gmail.com>
2021-10-13 09:55:27 -0700

    Cleanup carousel

--- react-client/src/SimpleCarousel/SimpleCarousel.js
+++ react-client/src/SimpleCarousel/SimpleCarousel.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Carousel, CarouselItem, CarouselCaption, CarouselIndicators } from "reactstrap";
-import carousel from "./Carousel.module.css";
+import CarouselCss from "./Carousel.module.css";
+import "./Carousel.css";
 import cnames from "classnames";
-import ConsoleLogger from "../Utility/LoggerFuncs";
 // const util = require("util"); //? Can help debug JS objects and more! -> Obj example below... May not matter with improved browser tools though
 
 let images = []; //? Reactstrap carousel requires image array prop
@@ -22,19 +22,14 @@ class SimpleCarousel extends Component {
   //? Required to start carousel animation
   next() {
     if (this.animating) return;
-    const nextIndex =
-      this.state.activeIndex === images.length - 1
-        ? 0
-        : this.state.activeIndex + 1;
+    const nextIndex = this.state.activeIndex === images.length - 1 ? 0 : this.state.activeIndex + 1;
     this.setState({ activeIndex: nextIndex });
   }
 
   previous() {
     if (this.animating) return;
-    const nextIndex =
-      this.state.activeIndex === 0
-        ? images.length - 1
-        : this.state.activeIndex - 1;
+    const nextIndex = this.state.activeIndex === 0 ? 
+      images.length - 1 : this.state.activeIndex - 1;
     this.setState({ activeIndex: nextIndex });
   }
 
@@ -46,44 +41,24 @@ class SimpleCarousel extends Component {
   render() {
     const { activeIndex } = this.state;
     const projectImgs = this.props.images;
-    // ConsoleLogger("Here are the carousel images");
-    // ConsoleLogger(util.inspect(projectImgs, false, null, true));
-    ConsoleLogger("Current viewWidth: " + this.props.viewWidth);
+    
     const slides = projectImgs.map(image => {
       return (
-        <CarouselItem key={image.image_url}>
-          <img
-            className={cnames(
-              "img-fluid",
-              this.props.viewWidth >= 768
-                ? carousel.slide
-                : carousel.mobileSlide
-            )}
-            src={image.image_url}
-            alt={image.alt_text}
-          />
+        <CarouselItem key={ image.image_url }>
+          <img className={ cnames("img-fluid", this.props.viewWidth >= 768 ? CarouselCss.slide : CarouselCss.mobileSlide) }
+            src={ image.image_url } alt={ image.alt_text } />
           <CarouselCaption captionText="" />
         </CarouselItem>
       );
     });
 
     return (
-      <Carousel
-        activeIndex={activeIndex}
-        next={this.next}
-        previous={this.previous}
-        pause={false}
-        ride="carousel"
-        interval={false}
-        className={cnames(carousel.full, "px-4 mt-3")}
-      >
-        <CarouselIndicators
-          className="mx-4 mt-3 mb-0"
-          items={projectImgs}
-          activeIndex={activeIndex}
-          onClickHandler={this.goToIndex}
-        />
-        {slides}
+      <Carousel pause={false} next={ this.next } previous={ this.previous }
+        activeIndex={ activeIndex } ride="carousel" interval={false}
+        className={ cnames(CarouselCss.full, "px-4 mt-3") }>
+          <CarouselIndicators items={ projectImgs } className="mx-4 mt-3 mb-0"
+            activeIndex={ activeIndex } onClickHandler={ this.goToIndex } />
+          {slides}
       </Carousel>
     );
   }
