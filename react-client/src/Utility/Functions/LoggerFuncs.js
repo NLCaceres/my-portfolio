6d3679c55de2d1aa6c6b6cf55bc88d319c41dc86
NLCaceres <NLCaceres21@Gmail.com>
2021-10-18 01:06:39 -0700

    Avoid double logging + better explain

--- react-client/src/Utility/Functions/LoggerFuncs.js
+++ react-client/src/Utility/Functions/LoggerFuncs.js
@@ -1,11 +1,12 @@
 const ConsoleLogger = (message, componentName, lineNum) => {
   if (process.env.RACK_ENV === 'production' || process.env.REACT_APP_RACK_ENV === 'production') return; 
 
+  //* Make your own stack trace by passing a short component name and the line num from your editor
   const stackTrace = (componentName && lineNum) ? `Inside: ${componentName} Component on line #${lineNum}` : 
     (componentName) ? `Inside: ${componentName} Component` : '';
     
   if (typeof message === 'object' && message !== null) {
-    if (stackTrace !== '') console.log(stackTrace);
+    if (stackTrace !== '') { console.log(stackTrace); return; } //* If made a stack trace, log here only
     console.log(message);
   } else {
     console.log(`${stackTrace} - ${message}`)
