05e7de63888976fefb593c889528f87f50e3b781
NLCaceres <NLCaceres21@Gmail.com>
2021-10-19 15:23:43 -0700

    Attempt 1 to fix Heroku init/root load issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,19 @@ class ApplicationController < ActionController::Base
 
   #* Since root (localhost:3000/) fires this method, we can be sure we redirect to React locally / in dev
   #* Thanks to combination of root + "*path" route setup in config/routes, of course!
-  #? Following ONLY runs in local dev, in production, React will handle most redirects
+  # def fallback_redirect
+  #   if ENV.fetch('RAILS_ENV', 'development') == 'production'
+  #     render file: 'public/index.html' #? Send React app and let it handle the redirects
+  #   else #? Following runs in local dev & testing
+  #     redirect_to path: 'portfolio', port: 3000, only_path: false
+  #     #? 'only_path: false' ensures ENTIRE url is redirect url "http://localhost/foobar" vs "/foobar"
+  #   end
+  #   # render 'index' #? Would make a request to application/index.html.erb
+  # end
   def fallback_redirect
-    redirect_to path: 'portfolio', port: 3000, only_path: false
-    #? 'only_path: false' ensures ENTIRE url is redirect url "http://localhost/foobar" vs "/foobar"
-    # render 'index' #? Would make a request to application/index.html.erb
+    redirect_url = { path: 'portfolio', port: 3000, only_path: false } #? Except!() removes multiple keys
+    redirect_url.except!(:port, :only_path) if ENV.fetch('RAILS_ENV', 'development') == 'production'
+    redirect_to(redirect_url) #? 1st param = url_for() options
   end
   #* Following could be useful BUT no public/index.html until react creates in prod
   #* Instead the above fallback dev imitates prod redirects pretty well
