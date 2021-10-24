55e5ee3ec0279f60e75e89fdc73d11df38f7b05a
NLCaceres <NLCaceres21@Gmail.com>
2021-10-24 14:22:33 -0700

    Replace render with send_file for speed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
   #* Both root + "*path" routes (config/routes) point to the following method
   def fallback_redirect
-    if ENV.fetch('RAILS_ENV', 'development') == 'production'
-      render file: "#{Rails.root}/public/index.html" #? In prod, send React app & let it handle redirects
-    else #? In dev + tests, redirect to 'localhost:3000/', so we're actually serving the react app!
+    if ENV.fetch('RAILS_ENV', 'development') == 'production' #? Send React app index in prod, let it redirect
+      send_file "#{Rails.root}/public/index.html", type: 'text/html; charset=utf-8', disposition: 'inline', status: 200
+    else #? In dev + tests, redirect to 'localhost:3000/', so the React dev server can send the app
       redirect_to path: 'portfolio', port: 3000, only_path: false #? 1st param = url_for() options
       #? 'only_path: false' ensures ENTIRE url is redirect url "http://localhost/foobar" vs "/foobar"
     end
