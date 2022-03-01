ffc04bfbad064d1bf057c371592088312d905d09
NLCaceres <NLCaceres21@Gmail.com>
2022-03-01 11:22:19 -0800

    Remove unneeded css + update recaptcha & router usage

--- react-client/src/App/App.js
+++ react-client/src/App/App.js
@@ -1,7 +1,6 @@
-import throttle from "lodash.throttle";
+import throttle from "lodash/throttle";
 import ConsoleLogger from "../Utility/Functions/LoggerFuncs";
 import "./App.css";
-// import AppCss from "./App.module.css";
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
 import SimpleNavBar from "../SimpleNavbar/SimpleNavbar";
@@ -14,12 +13,14 @@ import Footer from "../Footer/Footer";
 import UnavailableFeatureAlert from "../Utility/Components/AlertUnavailableFeature";
 //import * as serviceWorker from "./serviceWorker";
 
-class App extends Component {
+class App extends Component { //todo Should refactor App as func component to show Recaptcha via improved ReactRouter hooks
   constructor(props) {
     super(props);
     this.state = {
       width: window.innerWidth,
       showModal: false,
+      //todo If App is a func Component then can show recaptcha via ReactRouter hooks in specific routes
+      showRecaptcha: false, 
       showAlert: false
     };
 
@@ -30,7 +31,8 @@ class App extends Component {
     this.updateWindowDimensions();
     window.addEventListener("resize", throttle(this.updateWindowDimensions(), 500));
     window.addEventListener("load", this.setRecaptchaBadge);
-  }  
+    this.setRecaptchaBadge(); //* If already set, then no code run
+  }
 
   componentWillUnmount() {
     window.removeEventListener("load", this.setRecaptchaBadge);
@@ -39,17 +41,33 @@ class App extends Component {
   }
 
   setRecaptchaBadge = () => { //? Arrow Functions are an alternative to using bind() in ES6 classes
-    if (!this.recaptchaBadge) { 
-      this.recaptchaBadge = document.getElementsByClassName('grecaptcha-badge')[0];
-      this.recaptchaBadge.style.display = "none";
+    if (!this.recaptchaBadge) {
+      const recaptchaBadgeCheck = document.getElementsByClassName('grecaptcha-badge'); //* returns Array
+      if (recaptchaBadgeCheck && recaptchaBadgeCheck.length > 0) {
+        this.recaptchaBadge = recaptchaBadgeCheck[0]; //* Should have 1 recaptcha so checking 0 index so should avoid undefined issue
+        if (window.location.pathname === '/contact-me') {
+          this.recaptchaBadge.style.display = 'block'
+          this.setState({ showRecaptcha: true });
+        } else {
+          this.recaptchaBadge.style.display = "none";
+          this.setState({ showRecaptcha: false });
+        }
+      }
     }
   }
-  updateRecaptchaVisibility = (modalOpening) => {
+  showRecaptcha = (shouldShow) => {
     if (this.recaptchaBadge) {
-      this.recaptchaBadge.style.display = (modalOpening) ? "block" : "none";
-      //? Setting a CSS attr to "" removes the attr (if invalid) whereas 'auto' (the default)
-      //? OR 'initial' may keep the attr and unintendedly style element
-      this.recaptchaBadge.style['z-index'] = (modalOpening) ? 1050 : ""; //? 1050 matches modal z-index
+      const showRecaptchaBadge = shouldShow ?? !this.state.showRecaptcha; //? Safe way to compute state based on prevState
+      this.setState({ showRecaptcha: showRecaptchaBadge }); //? Do NOT compute state based on prevState in setState call
+      if (showRecaptchaBadge) {
+        this.recaptchaBadge.style.display = "block"
+        //? Setting a CSS attr to "" removes the attr (if invalid) whereas 'auto' (the default)
+        //? OR 'initial' may keep the attr and unintendedly style element
+        this.recaptchaBadge.style['z-index'] = 1050//? 1050 matches modal z-index
+      } else {
+        this.recaptchaBadge.style.display = "none";
+        this.recaptchaBadge.style['z-index'] = ""; //? 1050 matches modal z-index
+      }
     }
   }
 
@@ -59,7 +77,7 @@ class App extends Component {
 
   modalOpen = (shouldShow = true) => { //* Called with false value by form submit method
     ConsoleLogger(`Opening modal ${shouldShow}`);
-    this.updateRecaptchaVisibility(shouldShow);
+    this.showRecaptcha(shouldShow);
     this.setState({ showModal: shouldShow });
   }
 
@@ -79,45 +97,30 @@ class App extends Component {
   render() {
     //* Prevent double redirects BUT on initial viewing redirect to /portfolio
     return (
-      <BrowserRouter>
-        <Body viewWidth={ this.state.width } submitContactForm={ this.submitContactForm }
-          showModal={ this.state.showModal } modalOpen={ this.modalOpen } showAlert={ this.state.showAlert } setShow={ this.showAlert } />
-        {/* //? React-Router works in 3 parts, Router > Switch > Route/Redirect. You ALWAYS need those 3 parts, nested like so  */}
-        <Switch>
-          {/* //? If you place any regular components in a switch, it'll override any Routes/Redirects that come after it */}
-          {/* //? Redirects NEED to be placed IN Switches same as Routes */}
-          <Redirect exact from="/" to="/portfolio/about-me" />
-          {/* //* Any redirects at basename='portfolio' level need to be handled there, any at root here */}
-        </Switch>
+      //? React-Router works in 3 parts, Router > Switch > Route/Redirect. You ALWAYS need those 3 parts, nested like so
+      <BrowserRouter> 
+        <SimpleNavBar viewWidth={ this.state.width } />
+      
+        <MainRoutes viewWidth={ this.state.width } showRecaptcha={ this.showRecaptcha }/>
+        
+        <UnavailableFeatureAlert show={ this.state.showAlert } setShow={ this.showAlert} />
+        
+        <Footer viewWidth={ this.state.width } modalOpen={ () => this.modalOpen(true) }/>
+        
+        <SimpleModal ID="contact-me" show={ this.state.showModal } onHide={ () => this.modalOpen(false) } title="Send Me a Message!"
+          headerClasses={`pt-2 pb-1`} titleClasses={`font-weight-bolder text-white`}>
+            <ContactPageForm onSubmitForm={ this.submitContactForm } />
+        </SimpleModal>
       </BrowserRouter>
     );
   }
 }
 
-const Body = props => {
-  return (
-    <BrowserRouter basename="/portfolio">
-      <SimpleNavBar viewWidth={ props.viewWidth } /> 
-      
-      <MainRoutes viewWidth={ props.viewWidth }/>
-      
-      <UnavailableFeatureAlert show={ props.showAlert } setShow={ props.setShow } />
-      
-      <Footer viewWidth={ props.viewWidth } modalOpen={ props.modalOpen }/>
-      
-      <SimpleModal ID="contact-me-modal" show={ props.showModal } onHide={ () => props.modalOpen(false) } title="Send Me a Message!"
-        headerClasses={`pt-2 pb-1`} titleClasses={`font-weight-bolder`}>
-          <ContactPageForm className={``} onSubmitForm={ props.submitContactForm }></ContactPageForm>
-      </SimpleModal>
-    </BrowserRouter>
-  );
-}
-
 const MainRoutes = props => {
-  const paths = ['/iOS', '/android', '/front-end', '/back-end', '/about-me'];
+  const paths = ['iOS', 'android', 'front-end', 'back-end', 'about-me'];
   const mainRoutes = paths.map((pathStr) => {
     return (
-      <Route exact path={pathStr} key={pathStr} render={ routeProps =>
+      <Route exact path={`/portfolio/${pathStr}`} key={`/${pathStr}`} render={ routeProps =>
         (<PostListView viewWidth={ props.viewWidth } location={ routeProps.location } />) }
       />
     )
@@ -126,11 +129,14 @@ const MainRoutes = props => {
     <Switch>
       { mainRoutes }
       
-      <Route exact path="/contact-me" component={ ContactPage } />
-      <Redirect exact from="/" to="/about-me" /> {/*//* Without this route before the next redirect, always end up at 404 */}
+      <Route exact path="/contact-me" >
+        <ContactPage onMount={ props.showRecaptcha }/>
+      </Route>
+      <Route exact path="/" render={() => <Redirect to="/portfolio/about-me" />} /> 
+      {/*//* Need above 'home' route before the next redirect, or always get 404. Shows importance of order of routes/redirects */}
 
-      <Route exact path="/not-found" component={ NotFoundPage } />
-      <Redirect to="/not-found"/> {/* //? Redirects, if placed last, can be fallbacks just like Routes! */}
+      <Route exact path="/not-found" component={ NotFoundPage } />  {/*//? Use component prop if routeProps need to be injected */}
+      <Route render={() => <Redirect to="/not-found"/>} /> {/*//? Redirects placed last act as fallbacks */}
     </Switch>
   );
 };
