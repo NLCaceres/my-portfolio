6ca4154bd36885606a8611dfbc03b9ad2c983005
NLCaceres <NLCaceres21@Gmail.com>
2022-03-13 03:16:35 -0700

    Fix clearInterval spy test typo + cleanup

--- react-client/src/SimpleCarousel/SimpleCarousel.test.js
+++ react-client/src/SimpleCarousel/SimpleCarousel.test.js
@@ -5,45 +5,36 @@ import SimpleCarousel from "./SimpleCarousel";
 import { ProjectImageFactory } from '../Utility/Functions/Tests/ProjectFactory';
 import { averageTabletViewWidth, smallTabletHighEndWidth } from "../Utility/Constants/Viewports";
 
-const ImitateHoverHint = (carousel, alternating = false) => {
-  expect(carousel).not.toHaveClass('hovered-indicators');
-  //? Ordinarily would need act() to force state changes caused by jest's advanceTimers func BUT
-  //? Seemingly thanks to ReactTestingLibrary's version of render(), all good! No act() wrapper needed!
-  for (let i = 0; i <= 5; i++) {
-    jest.advanceTimersByTime(750);
-    if (alternating && i % 2 === 0) expect(carousel).toHaveClass('hovered-indicators');
-    else expect(carousel).not.toHaveClass('hovered-indicators');
-  }
-}
-
 describe('render a simple react-bootstrap carousel', () => {
-  let imageSet;
-  beforeEach(() => {
-    imageSet = [ProjectImageFactory.create(), ProjectImageFactory.create()];
-  })
+  const imageSet = [ProjectImageFactory.create(), ProjectImageFactory.create()];
   describe("that has indicators", () => {
     test("that flash on tablets and wider screens via set/clearInterval", () => {
       jest.useFakeTimers(); //? Setup to test to setInterval (or setTimeout if it was used)
       const setIntervalSpy = jest.spyOn(window, 'setInterval');
       const clearIntervalSpy = jest.spyOn(window, 'clearInterval');
       const { unmount } = render(<SimpleCarousel images={imageSet} viewWidth={averageTabletViewWidth} />)
+      expect(setIntervalSpy).toHaveBeenCalledTimes(1); 
+      expect(clearIntervalSpy).not.toHaveBeenCalled();
+
       const carouselRootElem = screen.getByTestId('simple-carousel');
-      ImitateHoverHint(carouselRootElem, true); //* Quick flashes + hover effect also possible
-      expect(setIntervalSpy).toHaveBeenCalledTimes(1);
-      jest.useRealTimers(); //? Cleanup/Reset
+      expect(carouselRootElem).not.toHaveClass('hovered-indicators'); //* Starts invisible
+      for (let i = 0; i < 6; i++) { //* 3 flashes of the indicators, on, off, on...
+        jest.advanceTimersByTime(750); //? ReactTestLib uses act() inside render() so rerenders from advanceTimers() work as expected!
+        (i % 2 === 0) ? expect(carouselRootElem).toHaveClass('hovered-indicators') : expect(carouselRootElem).not.toHaveClass('hovered-indicators');
+      }
+      expect(clearIntervalSpy).toHaveBeenCalledTimes(1); //* clearInterval called as hover hint animation finishes
       unmount();
-      expect(clearIntervalSpy).toHaveBeenCalledTimes(1);
+      expect(clearIntervalSpy).toHaveBeenCalledTimes(2); //* Called on unmount when bigger than mobile size
+      jest.useRealTimers(); //? Cleanup/Reset - Not too early or the interval spies get unmocked & undefined
       
       jest.useFakeTimers();
       const { unmount: secondUnmount } = render(<SimpleCarousel images={imageSet} viewWidth={smallTabletHighEndWidth} />);
       expect(setIntervalSpy).toHaveBeenCalledTimes(1); //* Spies not called at mobile size
-      expect(carouselRootElem).not.toHaveClass('hovered-indicators');
-      ImitateHoverHint(carouselRootElem); //* Regardless of time, indicators always shown + no hover effect
-      jest.useRealTimers();
+      expect(carouselRootElem).not.toHaveClass('hovered-indicators'); //* No flashes now or with timer advances
+      for (let i = 0; i < 6; i++) { jest.advanceTimersByTime(750); expect(carouselRootElem).not.toHaveClass('hovered-indicators'); }
       secondUnmount();
-      expect(clearIntervalSpy).toHaveBeenCalledTimes(1); //* Spies not called at mobile size
-      setIntervalSpy.mockRestore();
-      clearIntervalSpy.mockRestore();
+      expect(clearIntervalSpy).toHaveBeenCalledTimes(2); //* Spies not called at mobile size so remain at 2 total times called
+      jest.useRealTimers(); setIntervalSpy.mockRestore(); clearIntervalSpy.mockRestore(); //* Cleanup
     })
     test("that handles index changes via indicator buttons", async () => {
       const setStateMock = jest.spyOn(SimpleCarousel.prototype, 'setState');
@@ -72,16 +63,12 @@ describe('render a simple react-bootstrap carousel', () => {
     expect(carouselRoot).toHaveClass('full carousel slide', { exact: true });
     const carouselImageTags = screen.getAllByRole('img');
     expect(carouselImageTags).toHaveLength(2);
-    for (let imageTag of carouselImageTags) {
-      expect(imageTag).toHaveClass('img-fluid slide');
-    }
+    for (let imageTag of carouselImageTags) expect(imageTag).toHaveClass('img-fluid slide');
 
     rerender(<SimpleCarousel images={imageSet} viewWidth={smallTabletHighEndWidth} className="foobar" />);
     expect(carouselRoot).toHaveClass('full carousel slide foobar', { exact: true });
     expect(carouselImageTags).toHaveLength(2);
-    for (let imageTag of carouselImageTags) {
-      expect(imageTag).toHaveClass('img-fluid mobileSlide');
-    }
+    for (let imageTag of carouselImageTags) expect(imageTag).toHaveClass('img-fluid mobileSlide');
   })
   test("display captions if the property is available", () => {
     const { rerender } = render(<SimpleCarousel images={imageSet} viewWidth={averageTabletViewWidth} />)
