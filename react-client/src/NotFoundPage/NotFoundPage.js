a3f7c128d458f5fe35d2be7db01cd492a834cc25
NLCaceres <NLCaceres21@Gmail.com>
2023-01-28 17:53:16 -0800

    Improve NotFoundPage via useEffect over memo() + more CssModule usage

--- react-client/src/NotFoundPage/NotFoundPage.js
+++ react-client/src/NotFoundPage/NotFoundPage.js
@@ -1,32 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NotFoundCss from "./NotFoundPage.module.css"
+import ConsoleLogger from "../Utility/Functions/LoggerFuncs";
 
-//? Using react.memo allows shouldComponentUpdate like optimizing (see callback below)
-const NotFoundPage = React.memo(() => {
-  const randomImgSet = [
-    "https://imgur.com/uclpvfT.png",
-    "https://imgur.com/lNcHO0e.png",
-    "https://imgur.com/Of0gAOd.png",
-    "https://imgur.com/2EEuwzP.png",
-    "https://imgur.com/wkdXneC.png",
-    "https://imgur.com/DnGZrfn.png",
-    "https://imgur.com/UYxIDEk.png",
-    "https://imgur.com/KXnbSAi.png",
-    "https://imgur.com/Ow4Vn9x.png"
-  ];
-  const rand = Math.floor(Math.random() * 9);
-  const imgSrc = randomImgSet[rand];
-  
+//* NotFoundPage that renders a fun image on redirect/fallback from an unknown URL
+const NotFoundPage = () => {
+  //* Simplified approach to prevent rerenders that doesn't even need useLocation
+  //* PLUS allows a slick loading transition into the image via setTimeout() on setImgSrc()
+  const [imgSrc, setImgSrc] = useState(undefined);
+  useEffect(() => {
+    const randomImgSet = [
+      "https://imgur.com/uclpvfT.png",
+      "https://imgur.com/lNcHO0e.png",
+      "https://imgur.com/Of0gAOd.png",
+      "https://imgur.com/2EEuwzP.png",
+      "https://imgur.com/wkdXneC.png",
+      "https://imgur.com/DnGZrfn.png",
+      "https://imgur.com/UYxIDEk.png",
+      "https://imgur.com/KXnbSAi.png",
+      "https://imgur.com/Ow4Vn9x.png"
+    ];
+    const rand = Math.floor(Math.random() * 9);
+    setImgSrc(randomImgSet[rand])
+  }, []) //* No dependencies means this effect only runs onMount!
+
   return (
-    <div className="flex-grow-1" style={{ paddingLeft: "10px", paddingRight: "10px" }}>
+    <div className={`${NotFoundCss.container}`}>
       <h1>Sorry! Not Much to See Here!</h1>
-      <img src={ imgSrc } alt="A Cute Pup!" className={ `${NotFoundCss['img-thumbnail']}` } />
-      <h4 style={{ marginTop: "10px" }}>So Here's a Puppy to Make Up for It!</h4>
+      <img src={ imgSrc } alt="A Cute Pup!" className={ `${NotFoundCss.pupImage}` } />
+      <h4 className={`${NotFoundCss.caption}`}>So Here's a Puppy to Make Up for It!</h4>
     </div>
   );
-}, function (prevProps, nextProps) { //* Essentially an areEqual overriden function
-  //* Since no other props change, observe page url to prevent rerendering/img change when a modal appears
-  return prevProps.location?.pathname === nextProps.location?.pathname; //* Return true means props equal don't rerender
-});
+}
 
 export default NotFoundPage
--- react-client/src/NotFoundPage/NotFoundPage.test.js
+++ react-client/src/NotFoundPage/NotFoundPage.test.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import NotFoundPage from './NotFoundPage';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import NotFoundPage from "./NotFoundPage";
 
 describe("renders a basic but fun 'Not Found' Page", () => {
   test("with a div container inline-styled with a left + right padding", () => {
@@ -8,12 +8,12 @@ describe("renders a basic but fun 'Not Found' Page", () => {
     const titleTag = screen.getByText(/sorry/i);
     const containerTag = titleTag.parentElement;
     expect(containerTag).toBeInTheDocument();
-    expect(containerTag).toHaveStyle('padding-left: 10px; padding-right: 10px');
+    expect(containerTag).toHaveClass("container");
   })
   test("with an img tag with a random src + an 'img-thumbnail' cssModule", () => {
     const { rerender } = render(<NotFoundPage />);
-    const puppyImgTag = screen.getByRole('img', { name: /a cute pup/i });
-    expect(puppyImgTag).toHaveClass('img-thumbnail');
+    const puppyImgTag = screen.getByRole("img", { name: /a cute pup/i });
+    expect(puppyImgTag).toHaveClass("pupImage");
     const oldPuppyImgSrc = puppyImgTag.src;
     rerender(<NotFoundPage />); //* If same url (no navigation), a rerender won't change the img
     expect(puppyImgTag.src).toMatch(oldPuppyImgSrc);
@@ -28,7 +28,7 @@ describe("renders a basic but fun 'Not Found' Page", () => {
   })
   test("with a 'h4' tag styled with a top margin", () => {
     render(<NotFoundPage />)
-    const descriptionTag = screen.getByRole('heading', { name: /so here's a puppy to make up for it/i });
-    expect(descriptionTag).toHaveStyle('margin-top: 10px')
+    const descriptionTag = screen.getByRole("heading", { name: /so here's a puppy to make up for it/i });
+    expect(descriptionTag).toHaveClass("caption")
   })
 })
