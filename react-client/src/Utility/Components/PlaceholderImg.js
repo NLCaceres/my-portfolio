f16fb67199f8574d6a52dc039c7a6acf24bfce7d
NLCaceres <NLCaceres21@Gmail.com>
2023-01-31 00:08:32 -0800

    Destructure props. Add child prop, add spinner w/ load prop. Center text

--- react-client/src/Utility/Components/PlaceholderImg.js
+++ react-client/src/Utility/Components/PlaceholderImg.js
@@ -1,12 +1,14 @@
-import PlaceHolderImgCss from "./PlaceholderImg.module.css";
+import PlaceholderImgCss from "./PlaceholderImg.module.css";
+import AppSpinner from "./AppSpinner";
 
-//* Goal: Use placeholder with a lazy loading effect
-const PlaceHolderImg = props => {
+//* Simple placeholder that displays text and spinner if used during a loading event
+const PlaceholderImg = ({ className, children, loading }) => {
   return (
-    <div className={`${PlaceHolderImgCss.placeholderImg}`}>
-      <h2 className={`${PlaceHolderImgCss.placeholderText}`}>Project</h2>
+    <div className={`${PlaceholderImgCss.placeholderImg} ${className || ""}`}>
+      { loading && <AppSpinner className="mb-2" color="secondary" /> }
+      <h2 className={`${PlaceholderImgCss.placeholderText}`}>{ children || "Project" }</h2>
     </div>
   )
 }
 
-export default PlaceHolderImg;
+export default PlaceholderImg;
--- react-client/src/Utility/Components/PlaceholderImg.test.js
+++ react-client/src/Utility/Components/PlaceholderImg.test.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import PlaceHolderImg from './PlaceholderImg';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import PlaceholderImg from "./PlaceholderImg";
 
 describe("renders a div that looks like a placeholder img", () => {
   test("using a lookalike header tag in a container div", () => {
-    render(<PlaceHolderImg />)
-    const headerTag = screen.getByRole('heading');
+    render(<PlaceholderImg />);
+    const headerTag = screen.getByRole("heading");
     expect(headerTag).toBeInTheDocument();
-    expect(headerTag).toHaveClass('placeholderText');
-    expect(headerTag.parentElement).toHaveClass('placeholderImg');
+    expect(headerTag).toHaveClass("placeholderText");
+  })
+  test("with optional css classes for the container div", () => {
+    const { rerender } = render(<PlaceholderImg />);
+    const containerTag = screen.getByRole("heading").parentElement;
+    expect(containerTag).toHaveClass("placeholderImg");
+    
+    rerender(<PlaceholderImg className="foobar" />)
+    expect(containerTag).toHaveClass("placeholderImg foobar", { exact: true });
+  })
+  test("with either a default 'Project' text or text of choice", () => {
+    const { rerender } = render(<PlaceholderImg />);
+    const headerTag = screen.getByRole("heading", { name: "Project" });
+    expect(headerTag).toHaveTextContent("Project");
+
+    rerender(<PlaceholderImg>Foobar</PlaceholderImg>)
+    expect(headerTag).toHaveTextContent("Foobar");
+  })
+  test("with the option to render a spinner if an image is loading", () => {
+    const { rerender } = render(<PlaceholderImg />);
+    const missingAlert = screen.queryByRole("status");
+    expect(missingAlert).not.toBeInTheDocument();
+
+    rerender(<PlaceholderImg loading={true} />);
+    const alertFound = screen.getByRole("status");
+    expect(alertFound).toBeInTheDocument();
+
+    rerender(<PlaceholderImg />);
+    expect(alertFound).not.toBeInTheDocument();
   })
 })
