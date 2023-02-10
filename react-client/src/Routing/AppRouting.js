1b710e14504f21237a64633aeb15234234196273
NLCaceres <NLCaceres21@Gmail.com>
2023-02-09 20:42:16 -0800

    Add useTransition on route changes

--- react-client/src/Routing/AppRouting.js
+++ react-client/src/Routing/AppRouting.js
@@ -1,28 +1,45 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { animated, useTransition } from "@react-spring/web";
 import ContactPage from "../ContactMePage";
 import PostListView from "../PostListView/PostListView.js";
 import NotFoundPage from "../NotFoundPage/NotFoundPage";
+import AppRoutingCss from "./AppRouting.module.css";
+import ConsoleLogger from "../Utility/Functions/LoggerFuncs";
 
 const AppRouting = ({ viewWidth, submitContactForm }) => {
-  const paths = ["iOS", "android", "front-end", "back-end", "about-me"];
-  return ( //? 'Routes' must exist nested in a Router somewhere upstream. <Route /> must exist nested in a 'Routes' somewhere upstream
-    //? This ensures simple relative pathways where the base 'Routes' = "/" and all 'Routes' inside a <Route path="user"> takes on the prefix "/user"
-    <Routes>
-      { paths.map((pathStr) => { //? Route component dropped 'exact' prop + uses 'element' prop instead of 'children' or 'render' prop for components
-          return <Route path={`portfolio/${pathStr}`} key={pathStr} element={<PostListView viewWidth={ viewWidth } />} /> 
-        })
-      }
-      
-      <Route path="contact-me" element={<ContactPage viewWidth={ viewWidth } onSubmitForm={ submitContactForm } />} />
-      {/*//? 'path' doesn't require leading slash except in the Home: "/" path */}
-      <Route path="/" element={<Navigate to="portfolio/about-me" replace />} /> {/*//? Navigate w/ 'replace' replicates old Redirect functionality */}
-      {/*//* Need above 'home' route before the next Navigate, or always get 404, highlights the importance of order of routes */}
+  //! Animations
+  const location = useLocation();
+  const transitions = useTransition(location, {
+    keys: (item) => item.pathname, //? Prevents multiple Transition rerenders (limiting it nicely to two animating divs)
+    from: { transform: 'translate3d(200%,0,0)', opacity: 0 }, enter: { transform: 'translate3d(0%,0,0)', opacity: 1 }, 
+    leave: { transform: 'translate3d(-200%,0,0)', opacity: 0, position: "absolute" } //? Give space to newly entering Route
+  }); //* Routes do update before leaving, so "contact-me" briefly appears before exiting, while another is entering
+  //todo Maybe I could speed up the leave so it's less obvious?
 
-      <Route path="not-found" element={ <NotFoundPage /> } /> {/*//? Used for following wildcard "*" fallback route */}
-      <Route path="*" element={<Navigate to="not-found" replace/>} /> {/*//? Navigate runs a useEffect to nav to "/not-found" */}
-    </Routes>
-  );
+  //! Actual Routes
+  const paths = ["iOS", "android", "front-end", "back-end", "about-me"];
+  //? 'Routes' must exist nested in a Router somewhere upstream. <Route /> must exist nested in a 'Routes' somewhere upstream
+  //? This ensures simple relative pathways where the base 'Routes' = "/" and all 'Routes' inside a <Route path="user"> takes on the prefix "/user"
+  return transitions((style) => ( //* No container around the animated.div needed if only the exiting div is position: absolute (AKA out of the HTML doc's flow)
+    <animated.div className={AppRoutingCss.animator} style={style}>
+      <Routes> {/*//? DON'T add location from useLocation OR item: Location from transitions((style, item) => ()) to <Routes /> 'location' prop */}
+        {/*//! It seems ReactRouter may eventually add its own Animation API */}
+        { paths.map((pathStr) => { //? Route component dropped 'exact' prop + uses 'element' prop instead of 'children' or 'render' prop for components
+            return <Route path={`portfolio/${pathStr}`} key={pathStr} element={<PostListView viewWidth={ viewWidth } />} /> 
+          })
+        }
+        
+        <Route path="contact-me" element={<ContactPage viewWidth={ viewWidth } onSubmitForm={ submitContactForm } />} />
+        {/*//? 'path' doesn't require leading slash except in the Home: "/" path */}
+        <Route path="/" element={<Navigate to="portfolio/about-me" replace />} /> {/*//? Navigate w/ 'replace' replicates old Redirect functionality */}
+        {/*//* Need above 'home' route before the next Navigate, or always get 404, highlights the importance of order of routes */}
+
+        <Route path="not-found" element={ <NotFoundPage /> } /> {/*//? Used for following wildcard "*" fallback route */}
+        <Route path="*" element={<Navigate to="not-found" replace/>} /> {/*//? Navigate runs a useEffect to nav to "/not-found" */}
+      </Routes>
+    </animated.div>
+  ));
 };
 
 export default AppRouting;
--- react-client/src/Routing/AppRouting.test.js
+++ react-client/src/Routing/AppRouting.test.js
@@ -2,11 +2,16 @@ import React from "React";
 import AppRouting from "./AppRouting";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
+import { Globals } from "@react-spring/web";
 import ProjectFactory from "../Utility/Functions/Tests/ProjectFactory";
 import * as GetPostList from "../Api/ProjectAPI";
 
 jest.mock("../Utility/Components/TurnstileWidget", () => ({action, successCB, className }) => {
   return (<div><button type="button" onClick={() => { successCB("123") }}>Turnstile Verification Button</button></div>);
+})
+
+beforeAll(() => { //? Skip animating styles from Route Transitions, immediately finish their interpolation
+  Globals.assign({ skipAnimation: true }); //? So tests run quick BUT props are updated as expected!
 })
 
 describe("renders react-router-dom routes", () => {
@@ -17,54 +22,56 @@ describe("renders react-router-dom routes", () => {
   afterEach(() => { jest.restoreAllMocks() })
   
   test("mapping a home page to '/' and '/portfolio/about-me'", async () => {
-    const { unmount } = render(<MemoryRouter initialEntries={["/"]}><AppRouting /></MemoryRouter>);
+    //? Redirects seem to cause trouble when paired with React-Spring's useTransitions BUT by using my own key selection func +
+    //? not passing <Routes /> a location prop, I can avoid headaches in tests AND production (See AppRouting.js line 13 & 23)
+    const { unmount } = render(<MemoryRouter initialEntries={["/"]}> <AppRouting /> </MemoryRouter>);
     await screen.findByText(/about me/i); //* Allow placeholders to fade out and major/minor projects to render in
     unmount(); //* Must unmount to induce history changes in MemoryRouter during testing
 
-    const { unmount: unmountAboutMe } = render(<MemoryRouter initialEntries={["/portfolio/about-me"]}><AppRouting /></MemoryRouter>);
+    const { unmount: unmountAboutMe } = render(<MemoryRouter initialEntries={["/portfolio/about-me"]}> <AppRouting /> </MemoryRouter>);
     await screen.findByText(/about me/i); //* Should directly route to about me page
     unmountAboutMe();
 
-    render(<MemoryRouter initialEntries={["/about-me"]}><AppRouting /></MemoryRouter>);
+    render(<MemoryRouter initialEntries={["/about-me"]}> <AppRouting /> </MemoryRouter>);
     await screen.findByText(/Sorry/); //* THIS WILL NOT MATCH. It needs /portfolio prefix. Will get redirect instead
   })
 
   test("mapping out four main portfolio routes", async () => { //! iOS, Android, Front-End, Back-End
-    const { unmount: unmountAndroid } = render(<MemoryRouter initialEntries={["/portfolio/android"]}><AppRouting /></MemoryRouter>);
+    const { unmount: unmountAndroid } = render(<MemoryRouter initialEntries={["/portfolio/android"]}> <AppRouting /> </MemoryRouter>);
     await screen.findByText(/Android/); //* Should display Android in title header
     unmountAndroid();
 
-    const { unmount: unmountFrontend } = render(<MemoryRouter initialEntries={["/portfolio/front-end"]}><AppRouting /></MemoryRouter>);
+    const { unmount: unmountFrontend } = render(<MemoryRouter initialEntries={["/portfolio/front-end"]}> <AppRouting /> </MemoryRouter>);
     await screen.findByText(/Front-End/); //* Should display Front-End in title header
     unmountFrontend();
 
-    const { unmount: unmountBackend } = render(<MemoryRouter initialEntries={["/portfolio/back-end"]}><AppRouting /></MemoryRouter>);
+    const { unmount: unmountBackend } = render(<MemoryRouter initialEntries={["/portfolio/back-end"]}> <AppRouting /> </MemoryRouter>);
     await screen.findByText(/Back-End/); //* Should display Back-End in title header
     unmountBackend();
 
-    const { unmount: unmountIOS } = render(<MemoryRouter initialEntries={["/portfolio/iOS"]}><AppRouting /></MemoryRouter>);
+    const { unmount: unmountIOS } = render(<MemoryRouter initialEntries={["/portfolio/iOS"]}> <AppRouting /> </MemoryRouter>);
     await screen.findByText(/iOS/); //* Should display iOS in title header
     unmountIOS();
 
-    render(<MemoryRouter initialEntries={["/iOS"]}><AppRouting /></MemoryRouter>);
+    render(<MemoryRouter initialEntries={["/iOS"]}> <AppRouting /> </MemoryRouter>);
     await screen.findByText(/Sorry/); //* THIS WILL NOT MATCH. It needs /portfolio prefix. Just get redirect
   })
 
   test("mapping a contact me page", async () => {
-    const { unmount: unmountContactMe } = render(<MemoryRouter initialEntries={["/contact-me"]}><AppRouting /></MemoryRouter>);
+    const { unmount: unmountContactMe } = render(<MemoryRouter initialEntries={["/contact-me"]}> <AppRouting /> </MemoryRouter>);
     await screen.findByText(/Contact Me!/); //* Actual contact me page, not just the modal
     unmountContactMe();
 
-    render(<MemoryRouter initialEntries={["/portfolio/contact-me"]}><AppRouting /></MemoryRouter>);
+    render(<MemoryRouter initialEntries={["/portfolio/contact-me"]}> <AppRouting /> </MemoryRouter>);
     await screen.findByText(/Sorry/); //* Portfolio prefix not needed. Will redirect to not found page
   })
 
   test("mapping a fallback route to a not found page", async () => {
-    const { unmount: unmountRedirect } = render(<MemoryRouter initialEntries={["/foobar"]}><AppRouting /></MemoryRouter>);
+    const { unmount: unmountRedirect } = render(<MemoryRouter initialEntries={["/foobar"]}> <AppRouting /> </MemoryRouter>);
     await screen.findByText(/Sorry/); //* Should redirect to not found page
     unmountRedirect();
 
-    render(<MemoryRouter initialEntries={["/not-found"]}><AppRouting /></MemoryRouter>);
+    render(<MemoryRouter initialEntries={["/not-found"]}> <AppRouting /> </MemoryRouter>);
     await screen.findByText(/Sorry/); //* Goes straight to not found page
   })
 })
