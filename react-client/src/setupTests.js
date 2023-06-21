3897dded022784b5c9e678a7415045f6d4ec6e9d
NLCaceres <NLCaceres21@Gmail.com>
2023-06-20 18:42:54 -0700

    Add missing dummy ENV vars in setup of React tests for CI

--- react-client/src/setupTests.js
+++ react-client/src/setupTests.js
@@ -5,3 +5,8 @@
 import '@testing-library/jest-dom/extend-expect';
 
 global.console.warn = jest.fn(); //* Ignores ReactRouter basename warning (May hide other issues!)
+process.env = {
+  REACT_APP_RACK_ENV: "",
+  REACT_APP_CONTACTABLE: "true",
+  REACT_APP_ABOUT_ME_ID: "1"
+}
