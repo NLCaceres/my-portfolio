b466023a10ce884f482f76d71a22e4fe696bb1bd
NLCaceres <NLCaceres21@Gmail.com>
2023-07-27 22:01:45 -0700

    Reorganize to ONLY have use-case funcs + a root describe() in tests

--- react-client/src/Utility/Functions/ComputedProps.test.ts
+++ react-client/src/Utility/Functions/ComputedProps.test.ts
@@ -1,9 +1,7 @@
-import { CreateID, CleanAndKebabString, KebabCaseToTitleCase, KebabCaseToKebabTitleCase, 
-  IsEmpty, isString, Strip, TitleCase, CamelCaseToTitleCase, DropSpecialChars } from "./ComputedProps";
+import { CreateID, DropSpecialChars, CamelCaseToTitleCase, CleanAndKebabString, KebabCaseToTitleCase, KebabCaseToKebabTitleCase } from "./ComputedProps";
 
-describe("creates an appwide simple unique id", () => {
-  //* CreateID Func
-  test("using a static count property and class name prefix to return a tuple", () => {
+describe("provides functions to transform data into UI-ready forms", () => {
+  test("using a static count property and class name prefix to return a tuple that provides an ID and entity count", () => {
     const prefix = 'foobar';
     const [count, id] = CreateID(1, prefix);
     expect(count).toBe(2);
@@ -14,202 +12,8 @@ describe("creates an appwide simple unique id", () => {
     expect(countIdTuple).toContain(3);
     expect(countIdTuple).toContain("foobar-3");
   })
-})
 
-describe("provides an alternative method of calling trim to remove whitespace", () => {
-  expect(Strip("Hello World")).toBe("Hello World");
-  expect(Strip("Hello World  ")).toBe("Hello World");
-  expect(Strip("  Hello World")).toBe("Hello World");
-  expect(Strip("  Hello World  ")).toBe("Hello World");
-  expect(Strip("Hello   World")).toBe("Hello   World"); //* Whitespace in the middle stays the same
-  
-  expect(Strip(" World")).toBe("World");
-  expect(Strip(" World ")).toBe("World");
-  expect(Strip("World ")).toBe("World");
-})
-
-describe("removes html unsafe chars and kebabs the remainder of a phrase", () => {
-  //* CleanAndKebabString Func
-  test("removes html unsafe chars and lowers case", () => {
-    const testString = CleanAndKebabString('Foo!Bar/Bam:Dan?Fred[Tom`Kim{Em~')
-    expect(testString).toBe('foobarbamdanfredtomkimem');
-  })
-  test("kebabs spaces in between", () => {
-    const testString = CleanAndKebabString('foo bar');
-    expect(testString).toBe('foo-bar');
-  })
-})
-
-describe("provides simple string helpers", () => {
-  test("checking if a string as well as arrays are empty", () => {
-    expect(IsEmpty("Foobar")).toBe(false);
-    expect(IsEmpty(["Foobar"])).toBe(false);
-    expect(IsEmpty("Foo Bar")).toBe(false);
-    expect(IsEmpty(["Foo", "Bar"])).toBe(false);
-
-    expect(IsEmpty([])).toBe(true);
-    expect(IsEmpty("")).toBe(true);
-  })
-  test("checking if some variable is a string", () => {
-    expect(isString(0)).toBe(false);
-    expect(isString(0.0)).toBe(false);
-    expect(isString(1)).toBe(false);
-    expect(isString(1.0)).toBe(false);
-    expect(isString(2)).toBe(false);
-    expect(isString(2.0)).toBe(false);
-
-    expect(isString({})).toBe(false);
-    expect(isString({ foo: "bar" })).toBe(false);
-
-    expect(isString([])).toBe(false);
-    expect(isString(["foo"])).toBe(false);
-
-    expect(isString(true)).toBe(false);
-    expect(isString(false)).toBe(false);
-    expect(isString(null)).toBe(false);
-    expect(isString(undefined)).toBe(false);
-
-    expect(isString("foobar")).toBe(true);
-  })
-})
-
-describe("transform kebab-case phrase into uppercase whitespace delimited phrase", () => {
-  test("so 'the-cow-goes-moo' becomes 'The Cow Goes Moo", () => {
-    //* Used to consider all falsy values, but now only strings SHOULD ever be passed in
-    const emptyStrPhrase = KebabCaseToTitleCase("");
-    expect(emptyStrPhrase).toBe("");
-
-    const uppercaseSpacedPhrase = KebabCaseToTitleCase("the-cow-goes-moo");
-    expect(uppercaseSpacedPhrase).toBe("The Cow Goes Moo");
-    const normalSpacedPhrase = KebabCaseToTitleCase("the cow goes moo");
-    expect(normalSpacedPhrase).toBe("The cow goes moo"); //* Only "the" is capitalized
-    const uppercaseWord = KebabCaseToTitleCase("the");
-    expect(uppercaseWord).toBe("The");
-    const uppercaseTwoLetterWord = KebabCaseToTitleCase("is");
-    expect(uppercaseTwoLetterWord).toBe("Is");
-    const uppercaseLetter = KebabCaseToTitleCase("t");
-    expect(uppercaseLetter).toBe("T");
-    
-    //* Need to re-kebab certain phrases
-    const hyphenatedFrontEnd = KebabCaseToTitleCase("front-end");
-    expect(hyphenatedFrontEnd).not.toBe("Front-End");
-    expect(hyphenatedFrontEnd).toBe("Front End");
-    const hyphenatedBackEnd = KebabCaseToTitleCase("back-end");
-    expect(hyphenatedBackEnd).not.toBe("Back-End");
-    expect(hyphenatedBackEnd).toBe("Back End");
-  })
-})
-describe("transform kebab-case phrase into uppercase kebab-case phrase", () => {
-  test("so 'the-cow-goes-moo' becomes 'The-Cow-Goes-Moo", () => {
-    const emptyStr = KebabCaseToKebabTitleCase("");
-    expect(emptyStr).toBe("");
-
-    const uppercaseSpacedPhrase = KebabCaseToKebabTitleCase("the-cow-goes-moo");
-    expect(uppercaseSpacedPhrase).toBe("The-Cow-Goes-Moo");
-    const normalSpacedPhrase = KebabCaseToKebabTitleCase("the cow goes moo");
-    expect(normalSpacedPhrase).toBe("The cow goes moo"); //* Only "the" is capitalized
-    const uppercaseWord = KebabCaseToKebabTitleCase("the");
-    expect(uppercaseWord).toBe("The");
-    const uppercaseTwoLetterWord = KebabCaseToKebabTitleCase("is");
-    expect(uppercaseTwoLetterWord).toBe("Is");
-    const uppercaseLetter = KebabCaseToKebabTitleCase("t");
-    expect(uppercaseLetter).toBe("T");
-    
-    const hyphenatedFrontEnd = KebabCaseToKebabTitleCase("front-end");
-    expect(hyphenatedFrontEnd).toBe("Front-End");
-    const hyphenatedBackEnd = KebabCaseToKebabTitleCase("back-end");
-    expect(hyphenatedBackEnd).toBe("Back-End");
-  })
-})
-
-describe("transform camelCase phrase into uppercase whitespace delimited phrase", () => {
-  test("transforming 'theCowGoesMoo' into the 'The Cow Goes Moo'", () => {
-    const uppercaseSpacedPhrase = CamelCaseToTitleCase("theCowGoesMoo");
-    expect(uppercaseSpacedPhrase).toBe("The Cow Goes Moo");
-    const normalSpacedPhrase = CamelCaseToTitleCase("theCow goesMoo");
-    expect(normalSpacedPhrase).toBe("The Cow goes Moo"); //* "goes" is NOT capitalized since it's split as ["the", "Cow goes", "moo"]
-    const uppercaseWord = CamelCaseToTitleCase("the");
-    expect(uppercaseWord).toBe("The");
-    const uppercaseTwoLetterWord = CamelCaseToTitleCase("is");
-    expect(uppercaseTwoLetterWord).toBe("Is");
-    const uppercaseLetter = CamelCaseToTitleCase("t");
-    expect(uppercaseLetter).toBe("T");
-  })
-})
-
-describe("transform a string into a 'Title Case String'", () => {
-  test("handling strings whether a single char, a single word, or multiple words", () => {
-    expect(TitleCase("")).toBe(""); //* Empty strings get back empty strings
-    
-    //* Just returned back as an uppercase version
-    expect(TitleCase("b")).toBe("B");
-    expect(TitleCase("C")).toBe("C"); //* Remains the same
-    
-    //* After split() both strings should be contained in an array, then reduced to a Capital version
-    expect(TitleCase("fo")).toBe("Fo");
-    expect(TitleCase("Bar")).toBe("Bar"); //* Remains the same
-
-    expect(TitleCase("foo bar")).toBe("Foo Bar");
-    expect(TitleCase("Bar foo")).toBe("Bar Foo");
-
-    //* Single letter words in between are capitalized as well
-    expect(TitleCase("foo a bar")).toBe("Foo A Bar");
-    expect(TitleCase("Bar foo a")).toBe("Bar Foo A");
-
-    expect(TitleCase("abc def ghi")).toBe("Abc Def Ghi");
-    //* Other already Uppercased letters remain the same
-    expect(TitleCase("aBc DeF gHi")).toBe("ABc DeF GHi");
-  })
-  test("can skip words using a mapper obj if needed", () => {
-    //* Skips iOS by default
-    expect(TitleCase("iOS")).toBe("iOS");
-    expect(TitleCase("iOS hello")).toBe("iOS Hello");
-    expect(TitleCase("iOS hello World")).toBe("iOS Hello World");
-    expect(TitleCase("iOS hello world")).toBe("iOS Hello World");
-
-    //* Replaces the default obj turning "iOS" into "IOS"
-    expect(TitleCase("iOS hello", { hello: true })).toBe("IOS hello");
-    expect(TitleCase("hello iOS hello", { hello: true })).toBe("hello IOS hello");
-    expect(TitleCase("hello iOS Hello", { hello: true })).toBe("hello IOS Hello"); //* Already capitalized skipped-words remain so
-    expect(TitleCase("iOS hello", { hello: false })).toBe("IOS Hello");
-    
-    //* If true, then NO capitalization, If false then capitalize
-    expect(TitleCase("iOS hello", { iOS: false, hello: true })).toBe("IOS hello");
-    expect(TitleCase("iOS hello", { iOS: true, hello: true })).toBe("iOS hello");
-  })
-  test("provides additional transformation options", () => {
-    //* Additional transforms can be useful to append or prepend
-    expect(TitleCase("hello", {}, word => `${word}-`)).toBe("Hello-");
-    expect(TitleCase("hello world", {}, word => `${word}-`)).toBe("Hello-World-");
-    expect(TitleCase("hello world", {}, word => `_${word}`)).toBe("_Hello_World");
-    
-    //* Transforms don't occur on words that are skipped
-    expect(TitleCase("hello", { hello: true }, word => `${word}-`)).toBe("hello");
-    expect(TitleCase("hello world", { hello: true }, word => `${word}-`)).toBe("hello World-");
-    expect(TitleCase("hello world there", { hello: true }, word => `${word}-`)).toBe("hello World-There-");
-  })
-  test("handles an array of strings as if it was a simple space-separated phrase in a string", () => {
-    expect(TitleCase([])).toBe(""); //* Empty arrays are treated like empty strings
-
-    expect(TitleCase(["hello"])).toBe("Hello"); //* Each item is capitalized
-    expect(TitleCase(["hello", "world"])).toBe("Hello World"); //* And space-separated
-
-    //* Items are skipped as expected
-    expect(TitleCase(["hello", "world"], { hello: true })).toBe("hello World");
-    expect(TitleCase(["hello", "world", "hello"], { hello: true })).toBe("hello World hello");
-    expect(TitleCase(["Hello", "world", "hello"], { hello: true })).toBe("Hello World hello");
-
-    expect(TitleCase(["hello"], {}, word => `${word}-`)).toBe("Hello-"); //* Append a hyphen
-    expect(TitleCase(["hello", "world"], {}, word => `${word}-`)).toBe("Hello-World-"); //* Snake-case words
-
-    //* Skip transforms on words as well
-    expect(TitleCase(["hello"], { hello: true }, word => `${word}-`)).toBe("hello");
-    expect(TitleCase(["hello", "world"], { hello: true }, word => `${word}-`)).toBe("hello World-");
-  })
-})
-
-describe("drop special chars from a string's end", () => {
-  test("via Regex pattern matching replacement", () => {
+  test("drop special chars from a string's end via Regex pattern matching replacement", () => {
     expect(DropSpecialChars("")).toBe("");
     expect(DropSpecialChars("Foobar")).toBe("Foobar");
     expect(DropSpecialChars("Foo Bar")).toBe("Foo Bar");
@@ -236,5 +40,77 @@ describe("drop special chars from a string's end", () => {
 
     expect(DropSpecialChars("Foo Bar+-_~ ")).toBe("Foo Bar");
     expect(DropSpecialChars("+-_~ Foo Bar")).toBe("+-_~ Foo Bar");
+  })
+
+  test("transform camelCase phrase into uppercase whitespace delimited phrase", () => {
+    const uppercaseSpacedPhrase = CamelCaseToTitleCase("theCowGoesMoo");
+    expect(uppercaseSpacedPhrase).toBe("The Cow Goes Moo");
+    const normalSpacedPhrase = CamelCaseToTitleCase("theCow goesMoo");
+    expect(normalSpacedPhrase).toBe("The Cow goes Moo"); //* "goes" is NOT capitalized since it's split as ["the", "Cow goes", "moo"]
+    const uppercaseWord = CamelCaseToTitleCase("the");
+    expect(uppercaseWord).toBe("The");
+    const uppercaseTwoLetterWord = CamelCaseToTitleCase("is");
+    expect(uppercaseTwoLetterWord).toBe("Is");
+    const uppercaseLetter = CamelCaseToTitleCase("t");
+    expect(uppercaseLetter).toBe("T");
+  })
+
+  test("removes html unsafe chars and kebabs the remainder of a phrase", () => {
+    const testString = CleanAndKebabString('Foo!Bar/Bam:Dan?Fred[Tom`Kim{Em~')
+    expect(testString).toBe('foobarbamdanfredtomkimem');
+
+    const kebabString = CleanAndKebabString('foo bar');
+    expect(kebabString).toBe('foo-bar');
+
+    const kebabCleanString = CleanAndKebabString('Foo!Bar/Bam:Dan?Fred[Tom`Kim{Em~ fizz buzz')
+    expect(kebabCleanString).toBe('foobarbamdanfredtomkimem-fizz-buzz');
+
+    const kebabExtraCleanString = CleanAndKebabString('Foo!Bar/Bam:Dan?Fred[Tom`Kim{Em~ fizz:/ buzz')
+    expect(kebabExtraCleanString).toBe('foobarbamdanfredtomkimem-fizz-buzz');
+  })
+
+  test("transform kebab-case phrase into uppercase whitespace delimited phrase", () => {
+    //* Used to consider all falsy values, but now only strings SHOULD ever be passed in
+    const emptyStrPhrase = KebabCaseToTitleCase("");
+    expect(emptyStrPhrase).toBe("");
+
+    const uppercaseSpacedPhrase = KebabCaseToTitleCase("the-cow-goes-moo");
+    expect(uppercaseSpacedPhrase).toBe("The Cow Goes Moo");
+    const normalSpacedPhrase = KebabCaseToTitleCase("the cow goes moo");
+    expect(normalSpacedPhrase).toBe("The cow goes moo"); //* Only "the" is capitalized
+    const uppercaseWord = KebabCaseToTitleCase("the");
+    expect(uppercaseWord).toBe("The");
+    const uppercaseTwoLetterWord = KebabCaseToTitleCase("is");
+    expect(uppercaseTwoLetterWord).toBe("Is");
+    const uppercaseLetter = KebabCaseToTitleCase("t");
+    expect(uppercaseLetter).toBe("T");
+    
+    //* Need to re-kebab certain phrases
+    const hyphenatedFrontEnd = KebabCaseToTitleCase("front-end");
+    expect(hyphenatedFrontEnd).not.toBe("Front-End");
+    expect(hyphenatedFrontEnd).toBe("Front End");
+    const hyphenatedBackEnd = KebabCaseToTitleCase("back-end");
+    expect(hyphenatedBackEnd).not.toBe("Back-End");
+    expect(hyphenatedBackEnd).toBe("Back End");
+  })
+  test("transform kebab-case phrase into uppercase kebab-case phrase", () => {
+    const emptyStr = KebabCaseToKebabTitleCase("");
+    expect(emptyStr).toBe("");
+
+    const uppercaseSpacedPhrase = KebabCaseToKebabTitleCase("the-cow-goes-moo");
+    expect(uppercaseSpacedPhrase).toBe("The-Cow-Goes-Moo");
+    const normalSpacedPhrase = KebabCaseToKebabTitleCase("the cow goes moo");
+    expect(normalSpacedPhrase).toBe("The cow goes moo"); //* Only "the" is capitalized
+    const uppercaseWord = KebabCaseToKebabTitleCase("the");
+    expect(uppercaseWord).toBe("The");
+    const uppercaseTwoLetterWord = KebabCaseToKebabTitleCase("is");
+    expect(uppercaseTwoLetterWord).toBe("Is");
+    const uppercaseLetter = KebabCaseToKebabTitleCase("t");
+    expect(uppercaseLetter).toBe("T");
+    
+    const hyphenatedFrontEnd = KebabCaseToKebabTitleCase("front-end");
+    expect(hyphenatedFrontEnd).toBe("Front-End");
+    const hyphenatedBackEnd = KebabCaseToKebabTitleCase("back-end");
+    expect(hyphenatedBackEnd).toBe("Back-End");
   })
 })
--- react-client/src/Utility/Functions/ComputedProps.ts
+++ react-client/src/Utility/Functions/ComputedProps.ts
@@ -1,9 +1,25 @@
-//* Common transformations for props
+import { IsEmpty, TitleCase, WordModifierOptions } from "./StringExt";
 
+//* Common transformations for props */
+
+//! Quick Creation Helper
 //? Adding an explicit return type for this func helps reinforce it's a tuple, enabling easy typing when destructuring
 export function CreateID(objectCount: number, prefix: string): [number, string] {
   const updatedCount = ++objectCount;
   return [updatedCount, `${prefix}-${updatedCount}`];
+}
+
+//! Quick Removal Helper
+export function DropSpecialChars(str: string) {
+  return str.replace(/[+\-_~ ]+$/, ""); //? Matches "+-_~" (escapes the "-") AND whitespace
+}
+
+//! Casing Transformations
+//* Following used with obj keys to transform camelCase "aboutMe" to "About Me"
+export function CamelCaseToTitleCase(phrase: string, wordsToSkip?: WordModifierOptions) {
+  if (IsEmpty(phrase)) { return "" }
+  const words = phrase.split(/(?=[A-Z])/);
+  return TitleCase(words, wordsToSkip);
 }
 
 //* Remove HTML Unsafe Chars then replace whitespace with hyphens
@@ -12,16 +28,6 @@ export function CleanAndKebabString(title: string) {
   //? i.e. [!-/] = '!' (ASCII 33) to '/' (ASCII 47) whereas [!-/:-?] = '!' to '/' AND ':' (ASCII 58) to '?' (ASCII 63)
   const htmlAttrUnsafeChars = new RegExp('[!-/:-?[-`{-~]+', 'g');
   return (title) ? title.toLowerCase().replace(htmlAttrUnsafeChars, '').replace(/ /g, '-') : ''
-}
-
-export function IsEmpty<T>(value: string | Array<T>) {
-  return value.length === 0;
-}
-
-//* In practice, trims concatenated className strings
-//* As a whole, simply provides an alternative option to using trim() on a string a la Ruby
-export function Strip(value: string) {
-  return value.trim();
 }
 
 //* Following used with URLs to transform kebab cased directories "about-me" to "About Me"
@@ -35,48 +41,4 @@ export function KebabCaseToKebabTitleCase(phrase: string, wordsToSkip?: WordModi
   const words = phrase.split("-");
   const kebabTitlePhrase = TitleCase(words, wordsToSkip, word => `${word}-`)
   return kebabTitlePhrase.slice(0, -1); //* Slice off the hanging "-" so "Some-Phrase-" becomes "Some-Phrase"
-}
-
-//* Following used with obj keys to transform camelCase "aboutMe" to "About Me"
-export function CamelCaseToTitleCase(phrase: string, wordsToSkip?: WordModifierOptions) {
-  if (IsEmpty(phrase)) { return "" }
-  const words = phrase.split(/(?=[A-Z])/);
-  return TitleCase(words, wordsToSkip);
-}
-
-//* Provide the word, and whether the modifier should be applied via a boolean
-type WordModifierOptions = {
-  [word: string]: boolean
-}
-
-export function isString(str: any): str is string {
-  return typeof str === "string";
-}
-
-//* Take a whole string and capitalize any words it encounters a la Ruby on Rails
-export function TitleCase(str: string | string[], wordsToSkip: WordModifierOptions = { iOS: true }, wordTransform?: (word: string) => string) {
-  if (str.length === 0) { return "" }
-  if (isString(str) && !!wordsToSkip[str]) { return str } //* Check this is a str + make sure it needs to capitalized
-  if (isString(str) && str.length === 1) { return str.charAt(0).toUpperCase() } //* Single letter that can be dealt with quickly
-
-  const wordsInStr = Array.isArray(str) ? str : str.split(" "); //* If string is multiple words
-  return wordsInStr.reduce((buildStr, currentStr, index) => {
-    if (!!wordsToSkip[currentStr]) {
-      return (index === wordsInStr.length - 1) ? (buildStr + currentStr) : (buildStr + `${currentStr} `);
-    }
-
-    const casedWord = (currentStr.length === 1)
-      ? currentStr.toUpperCase()
-      : currentStr.charAt(0).toUpperCase() + currentStr.slice(1);
-
-    if (wordTransform) { return buildStr + wordTransform(casedWord) }
-
-    //? Double ternary to form simple if-elseIf-else conditional return
-    return (index === wordsInStr.length - 1) ? (buildStr + casedWord) //* Check if last (or ONLY) word in splitStr[]
-      : (buildStr + `${casedWord} `);
-  }, "");
-}
-
-export function DropSpecialChars(str: string) {
-  return str.replace(/[+\-_~ ]+$/, ""); //? Matches "+-_~" (escapes the "-") AND whitespace
 }
