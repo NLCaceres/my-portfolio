cd890549c4c3167c0c3de80e21c10f9a3f01c345
NLCaceres <NLCaceres21@Gmail.com>
2023-12-03 02:56:29 -0800

    Add starter ESLint config for React with Typescript

---
+++ react-client/.eslintrc.cjs
@@ -0,0 +1,51 @@
+/* eslint-env node */
+
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+    node: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "sourceType": "module",
+  },
+  "plugins": [
+    "@typescript-eslint",
+    "react"
+  ],
+  "rules": {
+    "linebreak-style": ["error", "unix"],
+    "no-trailing-spaces": "error",
+    "indent": "off",
+    "@typescript-eslint/indent": ["error", 2],
+    "quotes": "off",
+    "@typescript-eslint/quotes": "error",
+    "semi": "off",
+    "@typescript-eslint/semi": "error",
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+  },
+  overrides: [
+    {
+      env: {
+        jest: true
+      },
+      files: ["*.test.tsx"],
+      rules: {
+        "react/display-name": "off",
+        "@typescript-eslint/no-non-null-assertion": "off"
+      }
+    }
+  ],
+};
