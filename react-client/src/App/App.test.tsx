fab0e473dbd590b13f6ae10e2c75f7ff724f003e
NLCaceres <NLCaceres21@Gmail.com>
2024-03-31 22:03:40 -0700

    Finish replacing Jest mock refs + fix Vitest timer issue with Testing-Lib's UserEvent

--- react-client/src/App/App.test.tsx
+++ react-client/src/App/App.test.tsx
@@ -1,3 +1,4 @@
+import { vi, type MockInstance } from "vitest";
 import { RouterProvider, createMemoryRouter } from "react-router-dom";
 import { Globals } from "@react-spring/web";
 import { screen, render, act, waitFor } from "@testing-library/react";
@@ -12,43 +13,63 @@ import * as GetPostList from "../Data/Api/ProjectAPI";
 import * as CommonAPI from "../Data/Api/Common";
 import * as Scroll from "../Utility/Functions/Browser";
 
-jest.mock("../ThirdParty/TurnstileWidget", () => ({ successCB }: TurnstileWidgetProps) => {
-  return (<div><button type="button" onClick={() => { successCB("123") }}>Turnstile Verification Button</button></div>);
-})
+vi.mock("../ThirdParty/TurnstileWidget", () => {
+  return {
+    default: ({ successCB }: TurnstileWidgetProps) => {
+      return (<div><button type="button" onClick={() => { successCB("123") }}>Turnstile Verification Button</button></div>);
+    }
+  }
+});
 
 beforeAll(() => { //? Skip animating styles from Route Transitions, immediately finish their interpolation
   Globals.assign({ skipAnimation: true }); //? So tests run quick BUT props are updated as expected!
-})
+
+  //? Workaround for Vitest since Testing-Lib/UserEvent uses Jest timers by default,
+  //? making the typical userEvent.setup({ advanceTimers: vi.advanceTimersByTime }) useless without the following
+  //? This workaround is not required unless using userEvent, ex: TurnstileWidget or Browser tests
+  // @ts-ignore: Ignore setting Jest since no type is available without the package itself
+  const _jest = globalThis.jest;
+
+  // @ts-ignore
+  globalThis.jest = {
+    // @ts-ignore
+    ...globalThis.jest,
+    advanceTimersByTime: vi.advanceTimersByTime.bind(vi)
+  };
+
+  // @ts-ignore
+  return () => void (globalThis.jest = _jest); //? Cleanup function to reset the global Jest
+});
 
 describe("renders the whole app", () => {
-  let ApiMock: jest.SpyInstance;
+  let ApiMock: MockInstance;
   beforeEach(() => {
     const majProject = ProjectFactory.create(); const minProject = ProjectFactory.create();
-    ApiMock = jest.spyOn(GetPostList, "default").mockResolvedValue({ majorProjects: [majProject], minorProjects: [minProject] });
-  })
-  afterEach(() => { jest.restoreAllMocks() })
-  describe("controls the opening of an app-wide alert element", () => {    
+    ApiMock = vi.spyOn(GetPostList, "default").mockResolvedValue({ majorProjects: [majProject], minorProjects: [minProject] });
+  });
+  afterEach(() => { vi.restoreAllMocks(); });
+  describe("controls the opening of an app-wide alert element", () => {
     test("showing a danger alert if contact-me submit button fires without permission", async () => {
-      const user = userEvent.setup({ advanceTimers: jest.advanceTimersByTime }); //? VERY IMPORTANT due to setTimeout being used internally!
-      jest.useFakeTimers(); //? OTHERWISE jest's fakeTimers will freeze userEvents entirely (like in line 58)
-      jest.spyOn(CommonAPI, "SendEmail").mockResolvedValue("123"); //* Invalid response so Turnstile thinks user is a computer
+      const user = userEvent.setup({ advanceTimers: vi.advanceTimersByTime }); //? VERY IMPORTANT due to setTimeout being used internally!
+      vi.useFakeTimers(); //? OTHERWISE Vitest's fakeTimers will freeze userEvents entirely (like in line 58)
+      vi.spyOn(CommonAPI, "SendEmail").mockResolvedValue("123"); //* Invalid response so Turnstile thinks user is a computer
       const router = createMemoryRouter(RouteList, { initialEntries: ["/portfolio/about-me"] });
       render(<RouterProvider router={router} />);
       expect(ApiMock).toHaveBeenCalledTimes(1);
 
       await submitContactForm(user); //* Despite invalid user, trying to submit anyway fails behind the scenes
 
-      const dangerAlert = await screen.findByRole("alert") //* Failing behind the scenes, the alert appears, providing feedback that email wasn't sent
+      const dangerAlert = await screen.findByRole("alert"); //* Failing behind the scenes, the alert appears, providing feedback that email wasn't sent
       expect(dangerAlert).toHaveClass("alert-danger"); //* Correct color red on unsuccessful
       expect(screen.getByText(/email wasn't sent/i)).toBeInTheDocument(); //* Email not sent title
       expect(screen.getByText(/back up and running/i)).toBeInTheDocument(); //* Not working as expected message
 
       completeAlertTimeoutDismiss();
-    })
+    });
     test("showing a success alert if contact-me submit button fires when expected", async () => {
-      const user = userEvent.setup({ advanceTimers: jest.advanceTimersByTime }); //? VERY IMPORTANT due to setTimeout being used internally!
-      jest.useFakeTimers(); //? OTHERWISE jest's fakeTimers will freeze userEvents entirely
-      jest.spyOn(CommonAPI, "SendEmail").mockResolvedValue( //* Must return a valid response for Turnstile to provide a success response
+      const user = userEvent.setup({ advanceTimers: vi.advanceTimersByTime }); //? VERY IMPORTANT due to setTimeout being used internally!
+      vi.useFakeTimers(); //? OTHERWISE Vitest's fakeTimers will freeze userEvents entirely
+      vi.spyOn(CommonAPI, "SendEmail").mockResolvedValue( //* Must return a valid response for Turnstile to provide a success response
         { success: true, "error-codes": [], "challenge_ts": "1:00pm", "message": "Successfully sent your email!" }
       );
       const router = createMemoryRouter(RouteList, { initialEntries: ["/portfolio/about-me"] });
@@ -63,10 +84,10 @@ describe("renders the whole app", () => {
       expect(screen.getByText(/successfully sent/i)).toBeInTheDocument(); //* Successfully sent message
 
       completeAlertTimeoutDismiss();
-    })
+    });
     test("controls if the footer's contact me button opens a 'Contact Me' modal or navigates to '/contact-me'", async () => {
-      const scrollSpy = jest.spyOn(Scroll, "SmoothScroll");
-      const useViewWidthSpy = jest.spyOn(ViewWidthContext, "default").mockReturnValue(smallDesktopLowEndWidth);
+      const scrollSpy = vi.spyOn(Scroll, "SmoothScroll");
+      const useViewWidthSpy = vi.spyOn(ViewWidthContext, "default").mockReturnValue(smallDesktopLowEndWidth);
       const user = userEvent.setup();
       const router = createMemoryRouter(RouteList, { initialEntries: ["/portfolio/about-me"] });
       const { rerender } = render(<RouterProvider router={router} />);
@@ -88,12 +109,12 @@ describe("renders the whole app", () => {
       const contactMeButtonLink = await screen.findByRole("button", { name: /contact me/i });
       await user.click(contactMeButtonLink); //* Now at large mobile width, so should work as a link, navigating to "/contact-me"
       //? W/out location prop on <Routes>, <Routes> updates B4 leaving MEANWHILE a duplicate is entering so have to wait for original to leave
-      await waitFor(() => { expect(screen.getAllByRole("heading", { name: /contact me!/i, level: 1 })).toHaveLength(1) });
+      await waitFor(() => { expect(screen.getAllByRole("heading", { name: /contact me!/i, level: 1 })).toHaveLength(1); });
       expect(screen.getByRole("heading", { name: /contact me!/i, level: 1 })).toBeInTheDocument(); //? Now only have one "Contact Me!"
       expect(scrollSpy).toHaveBeenCalledTimes(1); //* Smooth scroll occurs on route transition, NOT modal opening
-    })
-  })
-})
+    });
+  });
+});
 
 //! Helper Functions
 async function submitContactForm(user: UserEvent) { //* Provide a reusable way of firing the contact form
@@ -117,10 +138,10 @@ async function submitContactForm(user: UserEvent) { //* Provide a reusable way o
   await user.click(correctContactButton); //* Click the actual submit button (not the button used to open the modal)
 }
 function completeAlertTimeoutDismiss() { //! This tests if alert disappears on a 5s timeout
-  //? Even if jest.useFakeTimers() was called here, line 59 "user.click" OR App's "setShowAlert" seemed to freeze the test, failing it
-  act(() => { jest.advanceTimersByTime(6000) }); //? Meaning waitForElementToBeRemoved() w/ a "{timeout:5000}" option would be the only solution
-  expect(screen.queryByRole("alert")).not.toBeInTheDocument(); //? Which is flakey & limiting especially w/ Jest's default 5s per test time limit
+  //? Even if vi.useFakeTimers() was called here, line 59 "user.click" OR App's "setShowAlert" seemed to freeze the test, failing it
+  act(() => { vi.advanceTimersByTime(6000); }); //? Meaning waitForElementToBeRemoved() w/ a "{timeout:5000}" option would be the only solution
+  expect(screen.queryByRole("alert")).not.toBeInTheDocument(); //? Which is flakey & limiting especially w/ Vitest's default 5s per test time limit
   //? Instead, the only quirk is using act() w/ timerAdvance since it lets setTimeout async run a state update (App.js line 28 "setShowAlert")
-  jest.runOnlyPendingTimers();
-  jest.useRealTimers();
+  vi.runOnlyPendingTimers();
+  vi.useRealTimers();
 }
