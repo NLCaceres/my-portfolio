cd49eca4a9383cb5dba2f965e2a443e8a4208016
NLCaceres <NLCaceres21@Gmail.com>
2024-04-02 13:23:58 -0700

    Setup coverage in Vitest via V8 + drop Jest's coverage config

--- react-client/vitest.config.ts
+++ react-client/vitest.config.ts
@@ -9,6 +9,26 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     css: { modules: { classNameStrategy: "non-scoped" } },
-    setupFiles: "./src/setupTests.ts"
+    setupFiles: "./src/setupTests.ts",
+    coverage: {
+      reporter: ["text"],
+      reportsDirectory: "./coverage",
+      thresholds: {
+        lines: 90,
+        functions: 90,
+        branches: 90,
+        statements: 90
+      },
+      include: ["**/*.{js,jsx,ts,tsx}"],
+      exclude: [
+        "build/**",
+        "public/**",
+        "**/node_modules/**",
+        "src/index.tsx",
+        "src/serviceWorker.js",
+        "src/**/LoggerFuncs.ts",
+        "src/**/WarningSilencer.ts"
+      ]
+    }
   },
 })
