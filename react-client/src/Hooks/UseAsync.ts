1fefad855e87a3eb5f73b1db4d3211f1a0c07a73
NLCaceres <NLCaceres21@Gmail.com>
2024-04-11 10:06:25 -0700

    Update UseAsync hook to typescript

--- react-client/src/Hooks/UseAsync.ts
+++ react-client/src/Hooks/UseAsync.ts
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 @params: asyncFunc - Use to fetch some server data -- Tip: Usually best to pass in a useCallback-backed func to prevent endless rerenders
 @params: onSuccess - Callback to use data from the Promise -- Tip: Convenient to pass in the setFunc from useState
 * IsAlive - Check if component using this Hook has been unmounted/rerendered. If not alive, DON'T allow state update */
-export default function UseNullableAsync(asyncFunc, onSuccess) {
+export default function UseNullableAsync<T>(asyncFunc: () => Promise<T>, onSuccess?: (result?: T) => void): void {
   useEffect(() => {
     let isAlive = true;
     (async () => { //? Good trick to avoid old school promises.then(callback)
