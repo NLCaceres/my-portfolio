435b21d180708134a64cd1071d741527e7bca22b
NLCaceres <NLCaceres21@Gmail.com>
2024-04-23 11:21:38 -0700

    Add in new Rails 7.1 app defaults - In production, there are two changes to watch:   - The `public` folder being served is no longer controlled by Env Var. It defaults to true, letting Rails serve the folder.   - The Logger is also no longer controlled Env Var. It is on by default and logs as `info`.

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require 'rails/all' #? Base Rails
 # Pick the frameworks you want:
@@ -23,7 +23,12 @@ module ResumeReactRails
   #* Rails Root File
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
     #
