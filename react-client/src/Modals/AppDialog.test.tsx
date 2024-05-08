8841f438da976c9e5fb4ce023515db17cc448bed
NLCaceres <NLCaceres21@Gmail.com>
2024-05-07 17:56:23 -0700

    Add an accessible dialog component via A11y-Dialog's hook

---
+++ react-client/src/Modals/AppDialog.test.tsx
@@ -0,0 +1,62 @@
+import { render, screen } from "@testing-library/react";
+import AppDialog from "./AppDialog";
+import { MutableRefObject } from "react";
+import { A11yDialogInstance } from "react-a11y-dialog";
+
+describe("renders an accessible dialog view over the window", () => {
+  test("expecting a title for easier accessibility regardless if the title is visible", () => {
+    const mockRef: MutableRefObject<A11yDialogInstance | undefined> = { current: undefined };
+    const { rerender } = render(<AppDialog dialogRef={mockRef} title="Welcome Dialog">Hello World!</AppDialog>);
+    //* WHEN the dialog is hidden
+    const hiddenDialog = screen.getByRole("dialog", { hidden: true });
+    expect(hiddenDialog).toBeInTheDocument();
+    expect(hiddenDialog).toHaveAttribute("aria-hidden", "true");
+    //* THEN the title is not visible
+    expect(screen.queryByRole("heading", { level: 1 })).not.toBeInTheDocument();
+
+    mockRef.current!.show();
+    //* WHEN the dialog is made visible
+    const visibleDialog = screen.getByRole("dialog", { hidden: false, name: "Welcome Dialog" });
+    expect(visibleDialog).toBeInTheDocument();
+    expect(visibleDialog).not.toHaveAttribute("aria-hidden");
+    //* THEN the title is visible to the screen reader by its heading role
+    expect(screen.getByRole("heading", { level: 1, name: "Welcome Dialog" })).toBeInTheDocument();
+    expect(screen.getByText("Welcome Dialog")).toBeInTheDocument(); //* A11y name of the <h1> tag matches its text
+    //* Both the title and its parent header <div> have their normal CSS classes and styling
+    expect(screen.getByText("Welcome Dialog")).toHaveClass("title", { exact: true });
+    expect(screen.getByText("Welcome Dialog").parentElement).toHaveClass("header", { exact: true });
+    mockRef.current!.hide();
+
+    rerender(<AppDialog dialogRef={mockRef} title="Hidden Dialog" hideTitle>Hello World!</AppDialog>);
+    //* WHEN the dialog AND the title is hidden
+    const newHiddenDialog = screen.getByRole("dialog", { hidden: true });
+    expect(newHiddenDialog).toBeInTheDocument();
+    expect(newHiddenDialog).toHaveAttribute("aria-hidden", "true");
+    //* THEN the title is not visible
+    expect(screen.queryByRole("heading", { level: 1 })).not.toBeInTheDocument();
+
+    mockRef.current!.show();
+    //* WHEN the dialog is made visible BUT title is still hidden
+    const newVisibleDialog = screen.getByRole("dialog", { hidden: false });
+    expect(newVisibleDialog).toBeInTheDocument();
+    expect(newVisibleDialog).not.toHaveAttribute("aria-hidden");
+    //* THEN the title is STILL VISIBLE TO THE SCREEN READER, even if the parent header <div> is visually hidden
+    const titleElement = screen.getByRole("heading", { level: 1, name: "Hidden Dialog" });
+    expect(titleElement.parentElement).toHaveClass("header hidden", { exact: true });
+    expect(titleElement).toBeInTheDocument();
+    expect(titleElement).toHaveClass("sr-only", { exact: true }); //* AND even if the title is visually hidden
+  });
+  test("uses `A11y-Dialog`'s hook and the useId hook to setup the dialog functionality and accessibility", () => {
+    const mockRef: MutableRefObject<A11yDialogInstance | undefined> = { current: undefined };
+    render(<AppDialog dialogRef={mockRef} title="Welcome Dialog">Hello World!</AppDialog>);
+    mockRef.current!.show();
+    //* WHEN the dialog is rendered, THEN its ID ends with a "-dialog" suffix, has an aria-modal attribute
+    const dialog = screen.getByRole("dialog");
+    expect(dialog).toHaveAttribute("id", expect.stringContaining("-dialog"));
+    expect(dialog).toHaveAttribute("aria-modal", "true");
+    expect(dialog).toHaveClass("container"); //* AND it uses the expected "container" CSS module class
+
+    //* AND the title has a similar ID that adds an additional "-title" suffix to the "-dialog" suffix
+    expect(screen.getByText("Welcome Dialog")).toHaveAttribute("id", expect.stringContaining("-dialog-title"));
+  });
+});
---
+++ react-client/src/Modals/AppDialog.tsx
@@ -0,0 +1,38 @@
+import { type PropsWithChildren, type MutableRefObject, useId } from "react";
+import { createPortal } from "react-dom";
+import { type A11yDialogInstance, useA11yDialog } from "react-a11y-dialog";
+import AppDialogCss from "./AppDialog.module.css";
+
+type DialogProps = {
+  title: string //? Required to help assistive tech identify the dialog to users
+  hideTitle?: boolean,
+  dialogRef: MutableRefObject<A11yDialogInstance | undefined>
+};
+
+const AppDialog = ({ title, hideTitle = false, dialogRef, children }: PropsWithChildren<DialogProps>) => {
+  const dialogID = useId();
+  const [instance, attr] = useA11yDialog({ id: dialogID + "-dialog" });
+  dialogRef.current = instance ?? undefined;
+
+  const headerCSS = hideTitle ? `${AppDialogCss.header} ${AppDialogCss.hidden}` : AppDialogCss.header;
+  const titleCSS = hideTitle ? "sr-only" : AppDialogCss.title;
+
+  return createPortal(
+    <div {...attr.container} className={AppDialogCss.container}>
+      <div {...attr.overlay} className={AppDialogCss.overlay} />
+      <div {...attr.dialog} className={AppDialogCss.dialog}>
+        <div className={headerCSS}>
+          <h1 id={attr.title.id} className={titleCSS}>{title}</h1>
+          <button {...attr.closeButton} className={AppDialogCss.closeButton} aria-label="Close this dialog window" />
+        </div>
+
+        <div className={AppDialogCss.content}>
+          {children}
+        </div>
+      </div>
+    </div>,
+    document.body
+  );
+};
+
+export default AppDialog;
