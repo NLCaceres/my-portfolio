6e5fab24618c2ffbb5b2c32d79b827856986d4f5
NLCaceres <NLCaceres21@Gmail.com>
2024-07-13 15:15:13 -0700

    Fix Cookie getter when Cookie string is empty + format comments - If no cookies have been set by the document, then the Cookie string from `document.cookie` is simply "", resulting in an empty array of cookies split by the ";" delimiter OR a single empty string in the array. In either case, better to return `undefined`

--- react-client/src/Utility/Functions/Browser.test.ts
+++ react-client/src/Utility/Functions/Browser.test.ts
@@ -4,19 +4,22 @@ import { vi } from "vitest";
 describe("provides common functions used by the browser ", () => {
 
   describe("getting cookies from browser", () => {
-    //todo May need a delete cookie option for tests since they can lead to test pollution
+    // TODO: May need a delete cookie option for tests since they can lead to test pollution
     test("handling when there are no cookies and getting specific cookies otherwise", () => {
+      // - WHEN there are no cookies, the cookies string/split array is simply empty, contains only a single empty string
       const emptyCookie = GetCookie("the-cookie-is-a-lie");
-      expect(emptyCookie).toBe(undefined);
+      expect(emptyCookie).toBe(undefined); // - THEN return undefined
 
       document.cookie = "name=foobar;";
       document.cookie = "title=barfoo;";
+      // - WHEN the desired cookie does not exist
       const missingCookie = GetCookie("the-cookie-is-a-lie");
-      expect(missingCookie).toBe(undefined);
+      expect(missingCookie).toBe(undefined); // - THEN return undefined
 
       document.cookie = "CSRF-TOKEN=some-token;";
+      // - WHEN the desired cookie exists in the document cookies string
       const token = GetCookie("CSRF-TOKEN");
-      expect(token).toBe("some-token");
+      expect(token).toBe("some-token"); // - THEN return the desired cookie's value alone
     });
   });
 
--- react-client/src/Utility/Functions/Browser.ts
+++ react-client/src/Utility/Functions/Browser.ts
@@ -1,16 +1,16 @@
-//* Contains functions that handle basic Browser functionality like getting specific cookies!
+// - Contains functions that handle basic Browser functionality like getting specific cookies!
 
 export function GetCookie(name: string) {
   const splitCookies = document.cookie.split("; ");
-  if (splitCookies.length === 0) { return; } //* No cookies, then early return
-  //* Search thru each cookie key/val pair to find cookie based on name
+  if (splitCookies.length === 0 || splitCookies[0] === "") { return; } // - No cookies, then early return
+  // - Search thru each cookie key/val pair to find cookie based on name
   const foundCookie = splitCookies.find(cookie => cookie.startsWith(`${name}=`));
-  if (!foundCookie) { return; } //* Expected cookie not found, early return
-  //* Then splits the key and value into a [key, value] arr,
-  return foundCookie.split("=")[1]; //* Take only the value from index 1
+  if (!foundCookie) { return; } // - Expected cookie not found, early return
+  // - Then splits the key and value into a [key, value] arr,
+  return foundCookie.split("=")[1]; // - Take only the value from index 1
 }
 
-//? Adding slight delay allows Firefox/Safari to properly scroll to top of document
-export function SmoothScroll() { //? 100 works for Firefox BUT Safari is SUPER slow so 350 works best
+// ?: Adding slight delay allows Firefox/Safari to properly scroll to top of document
+export function SmoothScroll() { // ?: 100 works for Firefox BUT Safari is SUPER slow so 350 works best
   setTimeout(() => window.scroll({ top: 0, left: 0, behavior: "smooth" }), 350);
-} //? Could also use this function in AppRouting useTransition's onRest event BUT it seems a bit too delayed
+} // ?: Could also use this function in AppRouting useTransition's onRest event BUT it seems a bit too delayed
