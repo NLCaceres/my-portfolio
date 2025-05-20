579f61e3fd780809b8131c3da1f6e4fa16bf1a2d
NLCaceres <NLCaceres21@Gmail.com>
2025-05-20 15:21:41 -0700

    Convert blank string <img> URLs to `undefined` to avoid page reloads + format
    - Blank strings used as a default `src` for img tags can cause some browsers
    to make a new request for the home page of your site, slowing the app down
    and causing unnecessary full reloads
    - Shortened line length for comments that were easy to understand as well as
    where component props could be moved to their own line

--- react-client/src/AppImages/BackgroundLoadImage.tsx
+++ react-client/src/AppImages/BackgroundLoadImage.tsx
@@ -23,45 +23,47 @@ type BackgroundImageProps = {
 const BackgroundLoadImage = (
   { src, alt, placeholderText = "", onImgClick, onLoad, className, placeholderClass, placeholderTextStyle, imgClass, parentRef }: BackgroundImageProps) => {
   //! Animation Setup
-  //? Height/width in useResize snap to window dimensions if the user changes the window's size (like when tilting the phone to landscape)
+	//? useResize height/width fits to window size even if user alters the size, e.g. phone tilting
   const { height, width } = useResize({ config: config.molasses }); //? Use a default tension/friction setting to control animation speed
   const [fadeOutSpring, fadeOutAPI] = useSpring(() => ({ from: { opacity: 1 } })); //* Setup the spring to use later
   const [fadeInSpring, fadeInAPI] = useSpring(() => ({ from: { opacity: 0 } })); //? Always use the start state BUT no 'to' key yet!
-  const resizeAnimations = () => { height.start(500); width.start(500); }; //* Begin resizing container
+  const resizeAnimations = () => { height.start(500); width.start(500); }; //* Start on container
   const successAnimations = () => {
     resizeAnimations();
     const slowestSpring = { tension: 260 , friction: 260 };
-    fadeInAPI.start({ from: { opacity: 0 }, to: { opacity: 1 }, config: slowestSpring }); //* Fade in the img tag
-    //* ALSO fade out the placeholder THEN once it has faded out, unmount it by setLoading to false
+    fadeInAPI.start({ from: { opacity: 0 }, to: { opacity: 1 }, config: slowestSpring });
+    //* ALSO fade out the placeholder THEN once faded out, unmount it by setLoading(false)
     fadeOutAPI.start({ from: { opacity: 1 }, to: { opacity: 0 }, config: slowestSpring, onRest: () => setLoading(false) });
   };
   //! Actual state
   const [loading, setLoading] = useState(false);
   const [successfulLoad, setSuccessfulLoad] = useState(false);
-  //! Lifecycle functions - useEffect called onMount + unmount. loadFinished called when img is ready. loadFailed called when an error occurs
-  useEffect(() => { setLoading(true); }, [src]); //? Only perform onMount, unmount or if img src changes
+  //! Lifecycle functions - useEffect called onMount + unmount
+  useEffect(() => { setLoading(true); }, [src]); //? Call again if src changes
   const loadFinished = () => { //* event param not needed but can add in future if needed
-    setSuccessfulLoad(true); //* If loadFinished runs, we should have a visible image, so mark it and remove placeholder cover
+    setSuccessfulLoad(true); //* Mark successful img load & remove placeholder
     successAnimations();
     if (onLoad) { onLoad(true); } //* Run parent's callback with success flag
   };
   const loadFailed = () => {
-    if (src !== "") { resizeAnimations(); } //* Just resize the placeholder IF fail isn't a result of a bad URL aka empty string ""
+    if (src !== "") { resizeAnimations(); } // Resize placeholder if fail not due to bad URL
     setLoading(false); //* Let the hidden failed image behind it unmount, completely unseen
     if (onLoad) { onLoad(false); } //* Run parent's callback with fail flag
   };
 
   return (
-    <animated.div className={`${BackgroundLoadImageCss.container} ${className || ""}`} ref={parentRef} style={{ height, width }}>
-      { (loading || !successfulLoad) && /*//* Cover img tag when loading and display as backup if load failed or threw errors */
+    <animated.div className={`${BackgroundLoadImageCss.container} ${className || ""}`}
+                  ref={parentRef} style={{ height, width }}>
+      { (loading || !successfulLoad) && /* If loading, cover <img> or use as backup on fail */
         <PlaceholderImg loading={loading} className={`${BackgroundLoadImageCss.placeholder} ${placeholderClass || ""}`.trim()}
                         style={fadeOutSpring} textStyle={placeholderTextStyle}>
           { placeholderText }
         </PlaceholderImg>
       }
-      { (loading || successfulLoad) && /*//* Once loading is set true, let the image load! If the src url fails, then remove the img! */
-        <animated.img src={src} alt={alt} onClick={onImgClick} onLoad={loadFinished} onError={loadFailed}
-                      className={`${BackgroundLoadImageCss.photo} ${imgClass || ""}`.trim()} style={fadeInSpring} />
+      { (loading || successfulLoad) && /* Let <img> load & show on success, else remove on fail */
+        <animated.img src={(src == "") ? undefined : src} alt={alt} onClick={onImgClick}
+                      onLoad={loadFinished} onError={loadFailed} style={fadeInSpring}
+                      className={`${BackgroundLoadImageCss.photo} ${imgClass || ""}`.trim()} />
       }
     </animated.div>
   );
