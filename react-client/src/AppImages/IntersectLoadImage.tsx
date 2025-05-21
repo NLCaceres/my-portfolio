06c30adf07b0e843c04c3229d5c078add4c03178
NLCaceres <NLCaceres21@Gmail.com>
2025-05-21 01:47:16 -0700

    Format `<IntersectLoadImage>` to improve documentation + shorten line length
    - Documentation comment follows a similar format to the documentation of
    `<BackgroundLoadImage>`, a summary followed by explanation of its param prop object
    type and the properties of the prop object

--- react-client/src/AppImages/IntersectLoadImage.tsx
+++ react-client/src/AppImages/IntersectLoadImage.tsx
@@ -3,15 +3,22 @@ import BackgroundLoadImage from "./BackgroundLoadImage";
 import { useInView } from "@react-spring/web";
 
 type IntersectLoadImageProps = {
-  src: string, alt: string, placeholderText?: ReactNode, onImgClick?: () => void, className?: string, imgClass?: string
+  src: string, alt: string, placeholderText?: ReactNode,
+	onImgClick?: () => void, className?: string, imgClass?: string
 };
 
-/*//* An image that begins loading once the user crosses a threshold of the element containing the BackgroundLoadingImage used  */
+/** An <img> that loads once the user crosses a percentage-based threshold of the component
+ * @param {IntersectLoadImageProps} props IntersectLoadImage properties to handle:
+ * Setting up the <img> src, alt text, and click listening as well as styling via
+ * className and imgClass. Additionally, since the <img> won't begin loading until
+ * on-screen, the placeholderText property is displayed while the <img> loads underneath */
 const IntersectLoadImage = ({ src, alt, placeholderText, onImgClick, className, imgClass }: IntersectLoadImageProps) => {
-  const [ref, inView] = useInView({ amount: 0.6, once: true }); //* ref attaches to element while inView simply notifies us when the threshold is crossed
+	//* Use ref to attach to element and inView var to check if element threshold passed
+  const [ref, inView] = useInView({ amount: 0.6, once: true });
   return (
-    <BackgroundLoadImage src={inView ? src : ""} alt={alt} parentRef={ref} className={className}
-                         placeholderText={placeholderText} onImgClick={onImgClick} imgClass={imgClass} />
+    <BackgroundLoadImage src={inView ? src : ""} alt={alt} parentRef={ref}
+                         className={className} placeholderText={placeholderText}
+                         onImgClick={onImgClick} imgClass={imgClass} />
   );
 };
 
