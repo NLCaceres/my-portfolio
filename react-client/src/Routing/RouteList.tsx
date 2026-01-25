d1027192e6efe451cb538aa9ac507541985ede51
NLCaceres <NLCaceres21@Gmail.com>
2026-01-25 13:09:07 -0800

    Drop `react-router-dom` + un-needed example `TanStack` routes from `RouteList`
    - Cleans up the most important remaining refs to `react-router-dom`, leaving
    a few small ones left in `PostListView`
    - Leaves `SortProjects` func import commented SINCE it MIGHT be best to do
    the sorting from the TanStack route loader.

--- react-client/src/Routing/RouteList.tsx
+++ react-client/src/Routing/RouteList.tsx
@@ -1,64 +1,9 @@
-import { createBrowserRouter } from "react-router-dom";
 import App from "../App/App";
-//import ContactPage from "../ContactMePage";
 import PostListView from "../PostListView/PostListView";
-//import NotFoundPage from "../NotFoundPage/NotFoundPage";
-import { createRootRouteWithContext, createRoute, createRouter, getRouteApi, Navigate, notFound, /*useParams*/ } from "@tanstack/react-router";
+import { createRootRouteWithContext, createRoute, createRouter, getRouteApi, Navigate, notFound } from "@tanstack/react-router";
 import { AlertHandler } from "../AppAlert/AppAlert";
 import GetPostList from "../Data/Api/ProjectAPI";
-import { SortProjects } from "../Data/Models/Project";
-//import { KebabCaseToTitleCase } from "../Utility/Functions/ComputedProps";
-
-//? Useful for testing and to allow modification before creating a router
-export const RouteList = [{
-  path: "/",
-  Component: App,
-  children: [
-    { //? This route gets redirected to "portfolio/about-me" in PostListView
-      index: true, //? Render this child at the parent's route i.e. "/" in this case
-      Component: PostListView
-    },
-    //{ //? This might be the one route that needs a more dynamic approach
-    //  path: "contact-me",
-    //  Component: ContactPage
-    //},
-    { //? Might be useful to pre-fetch before rendering, show loadVersion while doing so, and use a NotFoundPage as a fallback
-      path: "portfolio/iOS",
-      Component: PostListView
-    },
-    {
-      path: "portfolio/android",
-      Component: PostListView
-    },
-    {
-      path: "portfolio/front-end",
-      Component: PostListView
-    },
-    {
-      path: "portfolio/back-end",
-      Component: PostListView
-    },
-    {
-      path: "portfolio/about-me",
-      Component: PostListView
-    },
-    //{
-    //  path: "not-found",
-    //  Component: NotFoundPage
-    //},
-    //{
-    //  path: "*",
-    //  Component: NotFoundPage
-    //}
-  ]
-}];
-
-//? createBrowser will rank routes so order shouldn't matter
-const Router = createBrowserRouter(RouteList);
-
-export default Router;
-
-//! TanStack Implementation
+// import { SortProjects } from "../Data/Models/Project";
 
 type AppRouterContext = {
 	showAlert: AlertHandler
@@ -100,43 +45,6 @@ const portfolioChildRoute = createRoute({
   },
   component: PostListView
 });
-//type portfolioChildPath = typeof portfolioChildPaths[number];
-//const portfolioComponent = (path: portfolioChildPath) => {
-//  const fullPath = `/portfolio/${path}` as const; // Treats a simple string as a template literal
-//  const data = getRouteApi(fullPath).useLoaderData();
-//  return (
-//    <div>
-//      <h1>{KebabCaseToTitleCase(path)}</h1>
-//      <h3 style={{color: "white"}}>{data?.majorProjects[0].title ?? ""}</h3>
-//    </div>
-//  );
-//};
-//const aboutMeRoute = createRoute({
-//	getParentRoute: () => portfolioRoute,
-//	path: "about-me", // Loader SHOULD include `async` & `await` despite no need since it speeds up
-//  loader: async (ctx) => await GetPostList(postListParam(ctx.route.path)), // TS type inference
-//	component: () => portfolioComponent("about-me")
-//});
-//const androidRoute = createRoute({
-//	getParentRoute: () => portfolioRoute,
-//	path: "android", loader: async (ctx) => await GetPostList(postListParam(ctx.route.path)),
-//	component: () => portfolioComponent("android")
-//});
-//const iOSRoute = createRoute({
-//	getParentRoute: () => portfolioRoute,
-//	path: "iOS", loader: async (ctx) => await GetPostList(postListParam(ctx.route.path)),
-//	component: () => portfolioComponent("iOS")
-//});
-//const backEndRoute = createRoute({
-//	getParentRoute: () => portfolioRoute,
-//	path: "back-end", loader: async (ctx) => await GetPostList(postListParam(ctx.route.path)),
-//	component: () => portfolioComponent("back-end")
-//});
-//const frontEndRoute = createRoute({
-//	getParentRoute: () => portfolioRoute,
-//	path: "front-end", loader: async (ctx) => await GetPostList(postListParam(ctx.route.path)),
-//	component: () => portfolioComponent("front-end")
-//});
 export const contactMeRouteAPI = getRouteApi("/contact-me");
 const contactMeRoute = createRoute({
 	getParentRoute: () => rootRoute,
@@ -148,9 +56,7 @@ const notFoundRoute = createRoute({
 }).lazy(() => import("../NotFoundPage/NotFoundPage").then((f) => f.lazyNotFoundRoute));
 const routeTree = rootRoute.addChildren([
 	indexRoute, contactMeRoute, notFoundRoute,
-	portfolioRoute.addChildren([portfolioChildRoute,
-		portfolioIndexRoute,// aboutMeRoute, androidRoute, iOSRoute, backEndRoute, frontEndRoute
-	])
+	portfolioRoute.addChildren([portfolioChildRoute, portfolioIndexRoute])
 ]);
 
 export const TanStackRouter = createRouter({
